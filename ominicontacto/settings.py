93ba3b36be569066ba0f98ec08fb1bc99dabc438
federicopeker <federicopeker@gmail.com>
2016-06-07 15:41:27 -0300

    agrega template dirs

--- ominicontacto/settings.py
+++ ominicontacto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ominicontacto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
