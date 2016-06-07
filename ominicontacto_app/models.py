5ea88484bac83d9e8b23674e8c0d4ed83e571e50
federicopeker <federicopeker@gmail.com>
2016-06-07 15:47:11 -0300

    user personalizado

--- ominicontacto_app/models.py
+++ ominicontacto_app/models.py
@@ -1,5 +1,61 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_agente = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_supervisor = models.BooleanField(default=False)
+
+    def get_agente_profile(self):
+        agente_profile = None
+        if hasattr(self, 'agenteprofile'):
+            agente_profile = self.agenteprofile
+        return agente_profile
+
+#     def get_patient_profile(self):
+#         patient_profile = None
+#         if hasattr(self, 'patientprofile'):
+#             patient_profile = self.patientprofile
+#         return patient_profile
+#
+#     def get_physiotherapist_profile(self):
+#         physiotherapist_profile = None
+#         if hasattr(self, 'physiotherapistprofile'):
+#             physiotherapist_profile = self.physiotherapistprofile
+#         return physiotherapist_profile
+#
+#     class Meta:
+#         db_table = 'auth_user'
+#
+#
+
+
+class Modulo(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class AgenteProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+    name = models.CharField(max_length=64)
+    sip_extension = models.CharField(max_length=128, blank=True, null=True)
+    sip_password = models.CharField(max_length=128, blank=True, null=True)
+    modulos = models.ManyToManyField(Modulo)
+
+#
+# class PatientProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     active = models.BooleanField(default=True)
+#     name = models.CharField(max_length=64)
+#
+#
+# class PhysiotherapistProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     active = models.BooleanField(default=True)
+#     name = models.CharField(max_length=64)
