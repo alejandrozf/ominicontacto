1854263f516b61941f59ddef181e1df7f8734266
federicopeker <federicopeker@gmail.com>
2016-06-07 15:46:07 -0300

    agrega login url

--- ominicontacto/urls.py
+++ ominicontacto/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('ominicontacto_app.urls')),
+    url(r'^accounts/login/$', auth_views.login),
+
 ]
