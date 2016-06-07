add1bf025016de0d0619ed9c5678a27aa2a30327
federicopeker <federicopeker@gmail.com>
2016-06-07 15:14:10 -0300

    modelo de user personalizado

--- ominicontacto/settings.py
+++ ominicontacto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ominicontacto_app',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'ominicontacto_app.User'
