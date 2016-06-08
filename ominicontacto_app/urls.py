6a8fc94665a3aa855614c303cd10aa07312b0bb2
federicopeker <federicopeker@gmail.com>
2016-06-08 16:43:01 -0300

    agrega template index

--- ominicontacto_app/urls.py
+++ ominicontacto_app/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('',
     url(r'^ajax/mensaje_recibidos/',
         'ominicontacto_app.views.mensajes_recibidos_view',
-        name='ajax_mensaje_recibidos')
+        name='ajax_mensaje_recibidos'),
+    url(r'^$', 'ominicontacto_app.views.index_view', name='index'),
 )
--- ominicontacto_app/views.py
+++ ominicontacto_app/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 
 from services.sms_services import SmsManager
 from django.http import JsonResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 def mensajes_recibidos_view(request):
@@ -13,3 +15,7 @@ def mensajes_recibidos_view(request):
     response = JsonResponse(service_sms.armar_json_mensajes_recibidos(mensajes))
     return response
 
+
+def index_view(request):
+    return render_to_response('index.html',
+                              context_instance=RequestContext(request))
