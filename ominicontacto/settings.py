c9b25f142d6b990a1a8c4832caebb3d45274823a
federicopeker <federicopeker@gmail.com>
2016-07-06 10:59:38 -0300

    mover template a la app

--- ominicontacto/settings.py
+++ ominicontacto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ominicontacto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(__file__),'templates')],
+        # 'DIRS': [os.path.join(os.path.dirname(__file__),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
