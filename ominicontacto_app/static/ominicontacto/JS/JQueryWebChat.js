bcbde0a31ae5275cc712174a933619d00b4e37c3
Pignatar0md <pignataromd@gmail.com>
2016-08-17 10:51:59 -0300

    chat between plugin and agent interface

--- ominicontacto_app/static/ominicontacto/JS/JQueryWebChat.js
+++ ominicontacto_app/static/ominicontacto/JS/JQueryWebChat.js
@@ -4,13 +4,19 @@ var user = '';
 var configuration = null;
 
 $(function() {
+	debugger;
+	configuration = {
+      uri : "sip:"+$("#sipExt").val()+"@172.16.20.219",
+      ws_servers : "wss://172.16.20.219:443",
+      password : $("#sipSec").val()
+    };
   $("#conf").click(function() {
     $("#modalAccountConfig").modal('show');
   });
   ua = new JsSIP.UA(configuration);
-    sesion = ua.start();
+  sesion = ua.start();
 
-  $("#register").click(function() {
+  	debugger;
     $("#sendMessage").prop('disabled', false);
     $("#chatMessage").prop('disabled', false);
     $("#modalAccountConfig").modal('hide');
@@ -35,9 +41,6 @@ $(function() {
         chatWindow.appendChild(liMensaje);
       } else {
         fromUser = document.getElementById("user");
-        /*endPos = fromUser.indexOf("@");
-        startPos = fromUser.indexOf(":");
-        fromUser = fromUser.substring(startPos+1,endPos);*/
         liMensaje = document.createElement("li");
         var msgToSend = document.getElementById("chatMessage").value;
         liMensaje = document.createElement("li");
@@ -56,9 +59,8 @@ $(function() {
       var imgCelda1 = document.createElement("img");
       var txtCelda2 = document.createTextNode(user);
       var radioCelda3 = document.createElement("input");
-      //imgCelda1.src="Img/greendot.png";
       debugger;
-      //imgCelda1.src="/static/ominicontacto/Img/greendot.png";
+      imgCelda1.src="static/ominicontacto/Img/greendot.png";
       radioCelda3.type="checkbox";
       radioCelda3.id=user;
       celda3.style.textAlign='right';
@@ -70,25 +72,13 @@ $(function() {
       fila.appendChild(celda3);
       document.getElementById("tbodyContacts").appendChild(fila);
     });
-  });
   $("#sendMessage").click(function() {
+  	debugger;
     var mensaje = $("#chatMessage").val();
     user = $("#user").val();
     if(mensaje !== "") {
-   //   if ($("#uri").val() === "2008") {
         ua.sendMessage("sip:2003@172.16.20.219", mensaje);
-     /* } if ($("#uri").val() === "2001") {
-        ua.sendMessage("sip:2002@172.16.20.219", mensaje);
-      }*/
       $("#chatMessage").val("");
     }
   });
-  //$("#saveConf").click(function() {
-    // esto seria un sip para recibir y realizar llamadas
-    configuration = {
-      uri : "sip:"+$("#sipExt").val()+"@172.16.20.219",
-      ws_servers : "wss://172.16.20.219:443",
-      password : $("#sipSec").val()
-    };
-  //});
 });
