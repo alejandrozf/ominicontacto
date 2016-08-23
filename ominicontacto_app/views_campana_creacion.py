5cffd82e1b78be15d87aeca777b1e602a235ba6f
federicopeker <federicopeker@gmail.com>
2016-08-23 12:55:18 -0300

    eliminags

--- ominicontacto_app/views_campana_creacion.py
+++ ominicontacto_app/views_campana_creacion.py
@@ -369,8 +369,6 @@ class FormularioDemoFormUpdateView(UpdateView):
     def get_object(self, queryset=None):
         campana = Campana.objects.get(pk=self.kwargs['pk_campana'])
         contacto = Contacto.objects.get(pk=self.kwargs['pk_contacto'])
-        print campana
-        print contacto
         return FormularioDemo.objects.get(campana=campana, contacto=contacto)
 
     def get_success_url(self):
