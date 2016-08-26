cd112ef138bf476f92c4afe883dd93c97a4dc0ba
federicopeker <federicopeker@gmail.com>
2016-08-26 12:55:06 -0300

    corrige bugs

--- ominicontacto_app/views_campana_creacion.py
+++ ominicontacto_app/views_campana_creacion.py
@@ -368,22 +368,24 @@ class FormularioDemoFormUpdateView(UpdateView):
 
     def get_object(self, queryset=None):
         campana = Campana.objects.get(pk=self.kwargs['pk_campana'])
-        contacto = Contacto.objects.get(id_cliente=self.kwargs['id_cliente'])
-        return FormularioDemo.objects.get(campana=campana, contacto=contacto)
+        return FormularioDemo.objects.get(campana=campana,
+                                          id_cliente=self.kwargs['id_cliente'])
 
     def dispatch(self, *args, **kwargs):
         campana = Campana.objects.get(pk=self.kwargs['pk_campana'])
         try:
-            Contacto.objects.get(id_cliente=self.kwargs['id_cliente'])
-        except Contacto.DoesNotExist:
+            FormularioDemo.objects.get(campana=campana,
+                                       id_cliente=self.kwargs['id_cliente'])
+        except FormularioDemo.DoesNotExist:
             return HttpResponseRedirect(reverse('formulario_nuevo',
                                                 kwargs={"pk_campana":
                                                 self.kwargs['pk_campana']}))
-        except Contacto.MultipleObjectsReturned:
+        except FormularioDemo.MultipleObjectsReturned:
             return HttpResponseRedirect(reverse('contacto_list_id_cliente',
                                                 kwargs={"id_cliente":
                                                 self.kwargs['id_cliente']}))
-        return super(FormularioDemoFormUpdateView, self).dispatch(*args, **kwargs)
+        return super(FormularioDemoFormUpdateView, self).dispatch(*args,
+                                                                  **kwargs)
 
     def get_success_url(self):
         return reverse(
