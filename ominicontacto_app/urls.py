40d1b55c00a335ccaf90ae6cb5353df4febe17d8
federicopeker <federicopeker@gmail.com>
2016-08-31 15:43:25 -0300

    elimina url repetida

--- ominicontacto_app/urls.py
+++ ominicontacto_app/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
         views.mensajes_recibidos_view,
         name='ajax_mensaje_recibidos'),
     url(r'^$', views.index_view, name='index'),
-    url(r'^$', 'ominicontacto_app.views.index_view', name='index'),
     url(r'^accounts/login/$', views.login_view, name='login'),
     url(r'^user/nuevo/$',
         login_required(views.CustomerUserCreateView.as_view()),
