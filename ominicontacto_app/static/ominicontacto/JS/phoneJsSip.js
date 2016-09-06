99cdb348f389d75b03a1784e1091e90e7572ca79
Pignatar0md <pignataromd@gmail.com>
2016-09-06 14:52:25 -0300

    fix auto_attend IN variable

--- ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
+++ ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
@@ -231,7 +231,7 @@ $(function() {
   						}
   		  			break;
   					case "INBOUND":
-  		  			var inboundTag = document.getElementById("auto_attend_INBOUND");
+  		  			var inboundTag = document.getElementById("auto_attend_IN");
   		  			if(inboundTag.value === "True") {
   		  				$("#modalReceiveCalls").modal('hide');
   			  			session_incoming.answer(options);
