708490d0f13c0aac3745a22a4246f43101f19f9c
Pignatar0md <pignataromd@gmail.com>
2017-04-03 16:27:20 -0300

    receive a notify from nodeEngine on facebook

---
+++ ominicontacto_app/static/ominicontacto/JS/facebookMessenger.js
@@ -0,0 +1,15 @@
+/*var socket = new WebSocket("ws://172.16.20.237:8082/");
+
+socket.onopen = function () {
+  console.log('socket has been opened');
+};
+
+socket.onmessage = function (evt) {
+  console.log('mensaje desde server: ' + evt.data);
+};*/
+
+/*var socket = io.connect('http://172.16.20.237:8082');
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});*/
