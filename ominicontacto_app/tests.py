aef4d61b5113835e37290e2d14e44b847ad2db57
Pignatar0md <pignataromd@gmail.com>
2017-06-30 12:13:39 -0300

    test that try to load login page

--- ominicontacto_app/tests.py
+++ ominicontacto_app/tests.py
@@ -7,13 +7,30 @@ from selenium.webdriver.common.keys import Keys
 
 class SimpleSeleniumTest(TestCase):
 
-    def test_python_site(self):
+    def test_pagina_principal_carga_ok(self):
         driver = webdriver.Chrome()
-        driver.get("http://www.python.org")
-        assert "Python" in driver.title
-        elem = driver.find_element_by_name("q")
-        elem.clear()
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        driver.get("https://172.16.20.90/accounts/login")
+        self.assertEquals(driver.title, "Logueo Usuario")
         driver.close()
+
+    # def test_python_site(self):
+    #     driver = webdriver.Chrome()
+    #     driver.get("http://www.python.org")
+    #     assert "Python" in driver.title
+    #     elem = driver.find_element_by_name("username")
+    #     elem.clear()
+    #     elem.send_keys("usuariodeSuper1")
+    #     elem.send_keys(Keys.RETURN)
+    #     assert "No results found." not in driver.page_source
+    #     driver.close()
+    #
+    # def test_python_site(self):
+    #     driver = webdriver.Chrome()
+    #     driver.get("http://www.python.org")
+    #     assert "Python" in driver.title
+    #     elem = driver.find_element_by_name("q")
+    #     elem.clear()
+    #     elem.send_keys("pycon")
+    #     elem.send_keys(Keys.RETURN)
+    #     assert "No results found." not in driver.page_source
+    #     driver.close()
