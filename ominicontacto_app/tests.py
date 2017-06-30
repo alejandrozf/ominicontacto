d4ef36b08cc05e3223b1dd5aa830eb1bb785bc41
Pignatar0md <pignataromd@gmail.com>
2017-06-30 12:46:10 -0300

    test that runs login agent

--- ominicontacto_app/tests.py
+++ ominicontacto_app/tests.py
@@ -13,16 +13,14 @@ class SimpleSeleniumTest(TestCase):
         self.assertEquals(driver.title, "Logueo Usuario")
         driver.close()
 
-    # def test_python_site(self):
-    #     driver = webdriver.Chrome()
-    #     driver.get("http://www.python.org")
-    #     assert "Python" in driver.title
-    #     elem = driver.find_element_by_name("username")
-    #     elem.clear()
-    #     elem.send_keys("usuariodeSuper1")
-    #     elem.send_keys(Keys.RETURN)
-    #     assert "No results found." not in driver.page_source
-    #     driver.close()
+    def test_login_agente_ok(self):
+        driver = webdriver.Chrome()
+        driver.get("https://172.16.20.90/accounts/login")
+        driver.find_element_by_name("username").send_keys("usuariodeSuper1")
+        driver.find_element_by_name("password").send_keys("098098zzz")
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        self.assertEquals(driver.current_url, "https://172.16.20.90/node/")
+        driver.close()
     #
     # def test_python_site(self):
     #     driver = webdriver.Chrome()
