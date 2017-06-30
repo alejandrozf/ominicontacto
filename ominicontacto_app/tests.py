2aa835340ec53ff4333f0ff38d4bc8ecfdf18644
Pignatar0md <pignataromd@gmail.com>
2017-06-30 11:51:56 -0300

    first test with selenium

--- ominicontacto_app/tests.py
+++ ominicontacto_app/tests.py
@@ -1,3 +1,19 @@
+
 from django.test import TestCase
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class SimpleSeleniumTest(TestCase):
+
+    def test_python_site(self):
+        driver = webdriver.Chrome()
+        driver.get("http://www.python.org")
+        assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
