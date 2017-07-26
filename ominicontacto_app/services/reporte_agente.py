49d9eabc72c1a4b4be01d5d1ca54df899ec7d6f8
federicopeker <federicopeker@gmail.com>
2017-07-26 12:46:35 -0300

    reporte de tiempos en agente

--- ominicontacto_app/services/reporte_agente.py
+++ ominicontacto_app/services/reporte_agente.py
@@ -9,7 +9,8 @@ import datetime
 from pygal.style import Style, RedBlueStyle
 
 from django.db.models import Count
-from ominicontacto_app.models import CalificacionCliente, Calificacion
+from ominicontacto_app.models import CalificacionCliente, Calificacion, Queuelog
+from ominicontacto_app.services.queue_log_service import AgenteTiemposReporte
 import logging as _logging
 
 logger = _logging.getLogger(__name__)
@@ -85,17 +86,112 @@ class EstadisticasAgenteService():
 
         return dato_agente
 
+    def calcular_tiempo_agente(self, agente, fecha_inferior, fecha_superior, campana):
+        eventos_pausa = ['PAUSEALL', 'UNPAUSEALL']
+        agente_nuevo = AgenteTiemposReporte(agente, None, None, None, 0, 0, None, None)
+
+        logs_time = Queuelog.objects.obtener_log_agente_pk_event_periodo_all(
+            eventos_pausa, fecha_inferior, fecha_superior, agente.id)
+        is_unpause = False
+        time_actual = None
+        for logs in logs_time:
+            if is_unpause and logs.event == 'PAUSEALL':
+                resta = time_actual - logs.time
+
+                if agente_nuevo.tiempo_pausa:
+                    agente_nuevo._tiempo_pausa += resta
+                else:
+                    agente_nuevo._tiempo_pausa = resta
+
+                is_unpause = False
+                time_actual = None
+            if logs.event == 'UNPAUSEALL':
+                time_actual = logs.time
+                is_unpause = True
+
+        print agente_nuevo.tiempo_pausa
+
+        eventos_sesion = ['ADDMEMBER', 'REMOVEMEMBER']
+
+        logs_time = Queuelog.objects.obtener_log_agente_pk_event_periodo_all(
+            eventos_sesion, fecha_inferior, fecha_superior, agente.id)
+        is_remove = False
+        time_actual = None
+        for logs in logs_time:
+            if is_remove and logs.event == 'ADDMEMBER':
+                resta = time_actual - logs.time
+
+                if agente_nuevo.tiempo_sesion:
+                    agente_nuevo._tiempo_sesion += resta
+                else:
+                    agente_nuevo._tiempo_sesion = resta
+
+                is_remove = False
+                time_actual = None
+            if logs.event == 'REMOVEMEMBER':
+                time_actual = logs.time
+                is_remove = True
+
+        eventos_llamadas = ['COMPLETECALLER', 'COMPLETEAGENT']
+
+        logs_time = Queuelog.objects.obtener_log_agente_campana_event_periodo(
+            eventos_llamadas, fecha_inferior, fecha_superior, agente.id, campana.id)
+        lista_tiempo_llamada = [int(log.data2) for log in logs_time]
+
+        agente_nuevo._tiempo_llamada = sum(lista_tiempo_llamada)
+
+        eventos_llamadas_perdidas = ['RINGNOANSWER']
+
+        logs_time = Queuelog.objects.obtener_log_agente_campana_event_periodo(
+            eventos_llamadas, fecha_inferior, fecha_superior, agente.id, campana.id)
+        logs_time_perdidas = Queuelog.objects.obtener_log_agente_campana_event_periodo(
+            eventos_llamadas_perdidas, fecha_inferior, fecha_superior, agente.id,
+            campana.id)
+
+        agente_nuevo._cantidad_llamadas_procesadas = logs_time.count()
+        agente_nuevo._cantidad_llamadas_perdidas = logs_time_perdidas.count()
+
+        eventos_llamadas = ['COMPLETECALLER', 'COMPLETEAGENT']
+
+        logs_time = Queuelog.objects.obtener_log_agente_campana_event_periodo(
+            eventos_llamadas, fecha_inferior, fecha_superior, agente.id, campana.id)
+        lista_tiempo_llamada = [int(log.data2) for log in logs_time]
+        logs_saliente = logs_time.filter(data4='saliente')
+        lista_tiempo_saliente = [int(log.data2) for log in logs_saliente]
+        cantidad_llamadas = logs_time.count()
+        cantidad_saliente = logs_saliente.count()
+        cantidad_asignadas = cantidad_llamadas - cantidad_saliente
+        tiempo_llamadas = sum(lista_tiempo_llamada)
+        tiempo_saliente = sum(lista_tiempo_saliente)
+        tiempo_asignadas = tiempo_llamadas - tiempo_saliente
+        media_asignadas = 0
+        if tiempo_asignadas > 0:
+            media_asignadas = tiempo_asignadas / cantidad_asignadas
+        media_salientes = 0
+        if tiempo_saliente > 0:
+            media_salientes = tiempo_saliente / cantidad_saliente
+
+        agente_nuevo._media_asignada = media_asignadas
+        agente_nuevo._media_saliente = media_salientes
+        print agente_nuevo.tiempo_pausa
+        return agente_nuevo
+
     def _calcular_estadisticas(self, campana, fecha_desde, fecha_hasta, agente):
         calificaciones_nombre, calificaciones_cantidad, total_asignados = \
             self.obtener_cantidad_calificacion(campana, fecha_desde,
                                                fecha_hasta, agente)
         agentes_venta = self.obtener_venta(campana, agente, fecha_desde,
                                            fecha_hasta)
+
+        agente_tiempo = self.calcular_tiempo_agente(agente, fecha_desde, fecha_hasta,
+                                                    campana)
+        print agente_tiempo.tiempo_pausa
         dic_estadisticas = {
             'agentes_venta': agentes_venta,
             'total_asignados': total_asignados,
             'calificaciones_nombre': calificaciones_nombre,
             'calificaciones_cantidad': calificaciones_cantidad,
+            'agente_tiempo': agente_tiempo
         }
         return dic_estadisticas
 
