c01121580aa5c09b0f431e808608ddd0a094fb7f
Pignatar0md <pignataromd@gmail.com>
2017-08-29 15:50:01 -0300

    correct autopause and autounpause to inbound calls

--- ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
+++ ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
@@ -509,31 +509,53 @@ $(function() {
     }
 
 		e.session.on("ended",function() {               // Cuando Finaliza la llamada
-			if(fromUser) {
-				if(fromUser.substring(4,0) != "0077") {
+			if(entrante) {
+				if(fromUser) { // fromUser es para entrantes
+					if(fromUser.substring(4,0) != "0077") {
+							if ($("#auto_attend_DIALER").val() == "True" && $("#auto_pause").val() == "True") {//Si es un agente predictivo
+								if(lastPause != "Online") {
+						      saveCall(callerOrCalled);
+						      num = '';
+						      $("#Pause").prop('disabled',true);
+						      $("#Resume").prop('disabled',false);
+						      $("#sipLogout").prop('disabled',false);
+						      updateButton(modifyUserStat, "label label-danger", lastPause);
+						    } else {
+						      $("#Pause").prop('disabled',false);
+						      $("#Resume").prop('disabled',true);
+						      $("#sipLogout").prop('disabled',false);
+						      updateButton(modifyUserStat, "label label-success", lastPause);
+						    }
+							}
+						/*$("#Pause").prop('disabled',false);
+				    $("#Resume").prop('disabled',true);
+				    $("#sipLogout").prop('disabled',false);
+						updateButton(modifyUserStat, "label label-success", "Online");
+						parar1();
+				    inicio2();*/
+					}
+				}
+			} else {
+				if(num) { // num para salientes
+					if(num.substring(4,0) != "0077") {
 						if ($("#auto_attend_DIALER").val() == "True" && $("#auto_pause").val() == "True") {//Si es un agente predictivo
 							if(lastPause != "Online") {
-					      saveCall(callerOrCalled);
-					      num = '';
-					      $("#Pause").prop('disabled',true);
-					      $("#Resume").prop('disabled',false);
-					      $("#sipLogout").prop('disabled',false);
-					      updateButton(modifyUserStat, "label label-danger", lastPause);
-					    } else {
-					      $("#Pause").prop('disabled',false);
-					      $("#Resume").prop('disabled',true);
-					      $("#sipLogout").prop('disabled',false);
-					      updateButton(modifyUserStat, "label label-success", lastPause);
-					    }
-						}
-					/*$("#Pause").prop('disabled',false);
-			    $("#Resume").prop('disabled',true);
-			    $("#sipLogout").prop('disabled',false);
-					updateButton(modifyUserStat, "label label-success", "Online");
-					parar1();
-			    inicio2();*/
-				}
-			}
+								saveCall(callerOrCalled);
+								num = '';
+								$("#Pause").prop('disabled',true);
+								$("#Resume").prop('disabled',false);
+								$("#sipLogout").prop('disabled',false);
+								updateButton(modifyUserStat, "label label-danger", lastPause);
+							} else {
+								$("#Pause").prop('disabled',false);
+								$("#Resume").prop('disabled',true);
+								$("#sipLogout").prop('disabled',false);
+								updateButton(modifyUserStat, "label label-success", lastPause);
+							}
+					  }
+					}
+			  }
+		  }
     });
 
   });
