65c10cde70871b0e0dfacda882a079d31378e196
Pignatar0md <pignataromd@gmail.com>
2017-09-01 15:10:30 -0300

    enhacenment auto_pauses and auto_unpauses

--- ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
+++ ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
@@ -512,7 +512,6 @@ $(function() {
 			if(entrante) {
 				if(fromUser) { // fromUser es para entrantes
 					if(lastPause != "Online") {
-						//saveCall(callerOrCalled);
 						num = '';
 						$("#Pause").prop('disabled',true);
 						$("#Resume").prop('disabled',false);
@@ -522,20 +521,20 @@ $(function() {
 						$("#Pause").prop('disabled',false);
 						$("#Resume").prop('disabled',true);
 						$("#sipLogout").prop('disabled',false);
-						updateButton(modifyUserStat, "label label-success", lastPause);
+						updateButton(modifyUserStat, "label label-success", "Online");
 					}
 					if(fromUser.substring(4,0) != "0077") {
-							if ($("#auto_attend_DIALER").val() == "True" && $("#auto_pause").val() == "True") {//Si es un agente predictivo
+							if ($("#auto_pause").val() == "True") {//Si es un agente predictivo
 								changeStatus(3, $("#idagt").val());
-						    num = "0077ACW";
-						    makeCall();
-						    entrante = false;
+						        num = "0077ACW";
+						        makeCall();
+						        entrante = false;
 								$("#Pause").prop('disabled',true);
 								$("#Resume").prop('disabled',false);
 								$("#sipLogout").prop('disabled',false);
 								updateButton(modifyUserStat, "label label-danger", "ACW");
 								parar1();
-						    inicio2();
+						        inicio2();
 								if($("#auto_unpause").val() != 0) {
 							    var timeoutACW = $("#auto_unpause").val();
 							    timeoutACW = timeoutACW * 1000;
@@ -548,43 +547,16 @@ $(function() {
 							    };
 							    setTimeout(toOnline, timeoutACW);
 							  }
-							} else if($("#auto_pause").val() == "True") {
-                              changeStatus(3, $("#idagt").val());
-					          num = "0077ACW";
-					          makeCall();
-					          entrante = false;
-							  $("#Pause").prop('disabled',true);
-							  $("#Resume").prop('disabled',false);
-							  $("#sipLogout").prop('disabled',false);
-							  updateButton(modifyUserStat, "label label-danger", "ACW");
-							  parar1();
-					          inicio2();
-					          if($("#auto_unpause").val() != 0) {
-								var timeoutACW = $("#auto_unpause").val();
-								timeoutACW = timeoutACW * 1000;
-								var toOnline = function() {
-									num = "0077UNPAUSE";
-									if($("#UserStatus").html() === "ACW") {
-										makeCall();
-										$("#Resume").trigger('click');
-									}
-								};
-								setTimeout(toOnline, timeoutACW);
-							  }
-					        }
-						/*$("#Pause").prop('disabled',false);
-				    $("#Resume").prop('disabled',true);
-				    $("#sipLogout").prop('disabled',false);
-						updateButton(modifyUserStat, "label label-success", "Online");
-						parar1();
-				    inicio2();*/
+							}
+					} else {
+					    reinicio3($("#horaC"), $("#minsC"), $("#segsC"));
 					}
 				}
 			} else {
 				if (num) { // num para salientes
 					if (num.substring(4,0) != "0077") {
+						saveCall(num);
 						if (lastPause != "Online") {
-							//saveCall(callerOrCalled);
 							num = '';
 							$("#Pause").prop('disabled',true);
 							$("#Resume").prop('disabled',false);
@@ -596,7 +568,7 @@ $(function() {
 							$("#sipLogout").prop('disabled',false);
 							updateButton(modifyUserStat, "label label-success", lastPause);
 						}
-						if ($("#auto_attend_DIALER").val() == "True" && $("#auto_pause").val() == "True") {//Si es un agente predictivo
+						if ($("#auto_pause").val() == "True") {//Si es un agente predictivo
 							changeStatus(3, $("#idagt").val());
 					        num = "0077ACW";
 					        makeCall();
@@ -619,10 +591,14 @@ $(function() {
 								};
 								setTimeout(toOnline, timeoutACW);
 							}
-					  }
+					    }
+					} else {
+					   reinicio3($("#horaC"), $("#minsC"), $("#segsC"));
 					}
-			  }
-		  }
+			    }
+		    }
+		parar3();
+		defaultCallState();
     });
 
   });
