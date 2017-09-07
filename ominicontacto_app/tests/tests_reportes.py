44d33e8e51ff7aca619cbd8ff2575c03b626d4a7
desarrollo <desarrollo@fede-dev.example.com>
2017-09-07 10:49:46 -0300

    - Algunos fixes menores en setUp de los tests

--- ominicontacto_app/tests/tests_reportes.py
+++ ominicontacto_app/tests/tests_reportes.py
@@ -21,19 +21,19 @@ class ReportesTests(OMLBaseTest):
     def setUp(self):
         self.campanas_dialer = CampanaFactory.create_batch(
             NRO_CAMPANAS_DIALER, type=Campana.TYPE_DIALER)
-        self.campana_entrantes = CampanaFactory.create_batch(
+        self.campanas_entrantes = CampanaFactory.create_batch(
             NRO_CAMPANAS_ENTRANTES, type=Campana.TYPE_ENTRANTE)
-        self.campana_entrantes = CampanaFactory.create_batch(
+        self.campanas_manuales = CampanaFactory.create_batch(
             NRO_CAMPANAS_MANUALES, type=Campana.TYPE_MANUAL)
 
         self.queues_campanas_dialer = QueuelogFactory.create_batch(
-            NRO_QUEUES_DIALER, campana_id=self.campanas_dialer.objects.first().pk)
+            NRO_QUEUES_DIALER, campana_id=self.campanas_dialer[0].pk)
 
         self.queues_campanas_entrantes = QueuelogFactory.create_batch(
-            NRO_QUEUES_ENTRANTES, campana_id=self.campanas_entrantes.objects.first().pk)
+            NRO_QUEUES_ENTRANTES, campana_id=self.campanas_entrantes[0].pk)
 
         self.queues_campanas_manuales = QueuelogFactory.create_batch(
-            NRO_QUEUES_MANUALES, campana_id=self.campanas_manuales.objects.first().pk)
+            NRO_QUEUES_MANUALES, campana_id=self.campanas_manuales[0].pk)
 
     def test_es_correcto_el_calculo_de_el_reporte_de_llamadas(self):
         pass
