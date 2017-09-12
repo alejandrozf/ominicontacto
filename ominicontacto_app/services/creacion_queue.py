f2e8f0970e79d22b5de9814af8c80fd3bf6ccdb4
federicopeker <federicopeker@gmail.com>
2017-09-12 18:17:55 +0300

    comenta error de cola  reload config asterisk

--- ominicontacto_app/services/creacion_queue.py
+++ ominicontacto_app/services/creacion_queue.py
@@ -45,18 +45,18 @@ class ActivacionQueueService(object):
             mensaje_error += ("Hubo un inconveniente al crear el archivo de "
                               "configuracion del dialplan de Asterisk. ")
 
-        try:
-            ret = self.reload_asterisk_config.reload_config()
-            if ret != 0:
-                #proceso_ok = False
-                mensaje_error += ("Hubo un inconveniente al intenar recargar "
-                                  "la configuracion de Asterisk. ")
-        except:
-            logger.exception("ActivacionQueueService: error al "
-                             " intentar reload_config()")
-            proceso_ok = False
-            mensaje_error += ("Hubo un inconveniente al realizar el reload "
-                              " de Asterisk. ")
+        # try:
+        #     ret = self.reload_asterisk_config.reload_config()
+        #     if ret != 0:
+        #         #proceso_ok = False
+        #         mensaje_error += ("Hubo un inconveniente al intenar recargar "
+        #                           "la configuracion de Asterisk. ")
+        # except:
+        #     logger.exception("ActivacionQueueService: error al "
+        #                      " intentar reload_config()")
+        #     #proceso_ok = False
+        #     mensaje_error += ("Hubo un inconveniente al realizar el reload "
+        #                       " de Asterisk. ")
 
         try:
             self.queues_config_creator.create_dialplan()
