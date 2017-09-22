c58c42a55edd99aa3aca46a7449c8d73d2698c02
Pignatar0md <pignataromd@gmail.com>
2017-09-22 12:07:06 -0300

    nullify fromUser on line 515

--- ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
+++ ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
@@ -511,18 +511,18 @@ $(function() {
 		e.session.on("ended",function() {               // Cuando Finaliza la llamada
 			if(entrante) {
 				if(fromUser) { // fromUser es para entrantes
-					if(lastPause = "Online" && fromUser.substring(4,0) != "0077") {
+					if(lastPause === "Online" && fromUser.substring(4,0) != "0077") {
 						saveCall(fromUser);
 						num = '';
 						$("#Pause").prop('disabled',true);
 						$("#Resume").prop('disabled',false);
 						$("#sipLogout").prop('disabled',false);
-						updateButton(modifyUserStat, "label label-danger", "Online");
+						updateButton(modifyUserStat, "label label-success", "Online");
 					} else {
 						$("#Pause").prop('disabled',false);
 						$("#Resume").prop('disabled',true);
 						$("#sipLogout").prop('disabled',false);
-						updateButton(modifyUserStat, "label label-success", lastPause);
+						updateButton(modifyUserStat, "label label-danger", lastPause);
 					}
 					if(fromUser.substring(4,0) != "0077") {
 							if ($("#auto_pause").val() == "True") {//Si es un agente predictivo
