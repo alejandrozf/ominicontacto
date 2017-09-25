9370f2874a6e90f9f916d388fae29ccd4f82e9f9
Alejandro <alejandro.zamora@freetechsolutions.com.ar>
2017-09-25 12:11:11 -0300

    - Agrega clase para exportar a csv garantizando que los datos sean UTF-8   de forma más compacta

--- ominicontacto_app/utiles.py
+++ ominicontacto_app/utiles.py
@@ -5,6 +5,9 @@
 from __future__ import unicode_literals
 
 from contextlib import contextmanager
+import csv
+import codecs
+import cStringIO
 import os
 import re
 import tempfile
@@ -237,3 +240,33 @@ def convert_fecha_datetime(fecha, final_dia=False):
 def convertir_ascii_string(cadena):
     """ Devuelve ascii ignorando caracteres extraños"""
     return cadena.encode('ascii', errors='ignore')
+
+
+class UnicodeWriter:            # tomado de https://docs.python.org/2/library/csv.html
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
