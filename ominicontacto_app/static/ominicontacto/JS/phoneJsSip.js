ba6990cc99a9b893a299875ba18bb082c6b29c9a
Pignatar0md <pignataromd@gmail.com>
2017-09-27 16:02:16 -0300

    replace js hold click by jquery hold click

--- ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
+++ ominicontacto_app/static/ominicontacto/JS/phoneJsSip.js
@@ -435,7 +435,7 @@ $(function() {
 		$("#onHold").click(function() {
 			if(flagHold) {
   	 		flagHold = false;
-  	 	  var ag = e.session.hold(true);
+  	 	  var ag = e.session.hold(false);
 				console.log("Hold clicked: " +ag);
   	 	} else {
   	 	  flagHold = true;
