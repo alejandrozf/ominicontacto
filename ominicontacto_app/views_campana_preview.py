e1cd68baabf524bde78f8a7b7f3ff50f839ec1ce
Alejandro <alejandro.zamora@freetechsolutions.com.ar>
2017-10-13 13:08:54 -0300

    - Agrega vista de modificación de campañas preview

--- ominicontacto_app/views_campana_preview.py
+++ ominicontacto_app/views_campana_preview.py
@@ -6,7 +6,7 @@ import logging as logging_
 
 from django.contrib import messages
 from django.core.urlresolvers import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from ominicontacto_app.models import BaseDatosContacto, Campana, Queue
 from ominicontacto_app.forms import CampanaPreviewForm
@@ -77,6 +77,61 @@ class CampanaPreviewCreateView(CreateView):
         )
         queue.save()
         return super(CampanaPreviewCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('campana_preview_list')
+
+
+class CampanaPreviewUpdateView(UpdateView):
+    """
+    Esta vista actualiza un objeto Campana.
+    """
+
+    model = Campana
+    template_name = 'campana_preview/campana_preview.html'
+    context_object_name = 'campana'
+    form_class = CampanaPreviewForm
+
+    def get_initial(self):
+        initial = super(CampanaPreviewUpdateView, self).get_initial()
+        campana = self.get_object()
+        initial.update({
+            'auto_grabacion': campana.queue_campana.auto_grabacion,
+            'detectar_contestadores': campana.queue_campana.detectar_contestadores})
+        return initial
+
+    def get_object(self, queryset=None):
+        return Campana.objects.get(pk=self.kwargs['pk_campana'])
+
+    def form_valid(self, form):
+        tipo_interaccion = form.instance.tipo_interaccion
+        if tipo_interaccion is Campana.FORMULARIO and \
+           not form.instance.formulario:
+            error = "Debe seleccionar un formulario"
+            return self.form_invalid(form, error=error)
+        elif tipo_interaccion is Campana.SITIO_EXTERNO and not form.instance.sitio_externo:
+            error = "Debe seleccionar un sitio externo"
+            return self.form_invalid(form, error=error)
+        form.save()
+        auto_grabacion = form.cleaned_data['auto_grabacion']
+        detectar_contestadores = form.cleaned_data['detectar_contestadores']
+        queue = self.object.queue_campana
+        queue.auto_grabacion = auto_grabacion
+        queue.detectar_contestadores = detectar_contestadores
+        queue.save()
+        return super(CampanaPreviewUpdateView, self).form_valid(form)
+
+    def form_invalid(self, form, error=None):
+
+        message = '<strong>Operación Errónea!</strong> \
+                . {0}'.format(error)
+
+        messages.add_message(
+            self.request,
+            messages.WARNING,
+            message,
+        )
+        return self.render_to_response(self.get_context_data())
 
     def get_success_url(self):
         return reverse('campana_preview_list')
