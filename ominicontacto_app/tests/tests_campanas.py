f24bd65930ac63335d048c67920b55ef1c4d3351
Alejandro <alejandro.zamora@freetechsolutions.com.ar>
2017-10-13 15:34:15 -0300

    - Agrega tests para ABM campañas preview

--- ominicontacto_app/tests/tests_campanas.py
+++ ominicontacto_app/tests/tests_campanas.py
@@ -5,9 +5,11 @@ Tests relacionados con las campañas
 """
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
+
 from ominicontacto_app.models import Campana
 
-from ominicontacto_app.tests.factories import CampanaFactory
+from ominicontacto_app.tests.factories import CampanaFactory, UserFactory
 
 from ominicontacto_app.tests.utiles import OMLBaseTest
 
@@ -18,8 +20,20 @@ from django.forms import ValidationError
 
 class CampanasTests(OMLBaseTest):
 
+    PWD = u'admin123'
+
     def setUp(self):
+        self.usuario_admin_supervisor = UserFactory(is_staff=True, is_supervisor=True)
+        self.usuario_admin_supervisor.set_password(self.PWD)
+        self.usuario_admin_supervisor.save()
+
         self.campana = CampanaFactory()
+        self.campana_activa = CampanaFactory(
+            estado=Campana.ESTADO_ACTIVA, type=Campana.TYPE_PREVIEW)
+        self.campana_borrada = CampanaFactory(
+            estado=Campana.ESTADO_BORRADA, oculto=False, type=Campana.TYPE_PREVIEW)
+
+        self.client.login(username=self.usuario_admin_supervisor.username, password=self.PWD)
 
     def test_campana_contiene_atributo_entero_positivo_llamado_objetivo(self):
         self.assertTrue(self.campana.objetivo >= 0)
@@ -38,3 +52,37 @@ class CampanasTests(OMLBaseTest):
 
     def test_tiempo_desconexion_campanas_preview(self):
         self.assertTrue(self.campana.tiempo_desconexion >= 0)
+
+    def test_usuarios_no_logueados_no_acceden_a_vista_lista_campanas_preview(self):
+        self.client.logout()
+        url = reverse('campana_preview_list')
+        response = self.client.get(url, follow=True)
+        self.assertTemplateUsed(response, u'registration/login.html')
+
+    def test_usuarios_no_logueados_no_acceden_a_vista_creacion_campanas_preview(self):
+        self.client.logout()
+        url = reverse('campana_preview_create')
+        response = self.client.post(url, follow=True)
+        self.assertTemplateUsed(response, u'registration/login.html')
+
+    def test_usuarios_no_logueados_no_acceden_a_vista_modificacion_campanas_preview(self):
+        self.client.logout()
+        url = reverse('campana_preview_update', args=[self.campana_activa.pk])
+        response = self.client.post(url, follow=True)
+        self.assertTemplateUsed(response, u'registration/login.html')
+
+    def test_usuarios_no_logueados_no_acceden_a_vista_eliminacion_campanas_preview(self):
+        self.client.logout()
+        url = reverse('campana_preview_delete', args=[self.campana_borrada.pk])
+        response = self.client.post(url, follow=True)
+        self.assertTemplateUsed(response, u'registration/login.html')
+
+    def test_usuarios_logueados_pueden_ver_lista_de_campanas_preview_activas(self):
+        url = reverse('campana_preview_list')
+        response = self.client.get(url, follow=True)
+        self.assertContains(response, self.campana_activa.nombre)
+
+    def test_usuarios_logueados_pueden_ver_lista_de_campanas_preview_borras(self):
+        url = reverse('campana_preview_list')
+        response = self.client.get(url, follow=True)
+        self.assertContains(response, self.campana_borrada.nombre)
