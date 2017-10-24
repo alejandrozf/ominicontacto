9f3f31f99de47e4df8649bd55211b63e49607269
lionite <felipe.macias@freetechsolutions.com.ar>
2017-10-24 17:00:02 -0300

    nuevos cambios, terminado parte voip

--- ansible/deploy/roles/oml_server/files/bin/deploy_prefreepbx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ejecutando Ansible en omni-voip Pre-Freepbx"
-ansible-playbook -s /etc/ansible/pre-freepbx/omni-debian -u freetech -K
-
-ansible-playbook -s /etc/ansible/pre-freepbx/omni-freepbx.yml -u root
