bd7c24d442c38460777fca02a7b87ac8390e126f
lionite <felipe.macias@freetechsolutions.com.ar>
2017-12-12 11:58:56 -0300

    cambio de oml_settings para tener omnisup en https apache y cambio de ruta en estaticos

--- ansible/deploy/roles/oml_server/templates/oml_settings_local_debian.py
+++ ansible/deploy/roles/oml_server/templates/oml_settings_local_debian.py
@@ -97,7 +97,7 @@ OML_GRABACIONES_URL = "http://{{ omnivoip_ip }}/grabaciones"
 # 'OML_SUPERVISION_URL': url donde se encuentra las grabaciones en elastix
 # ejemplo "http://172.16.20.222:8090/Omnisup/index.php"
 
-OML_SUPERVISION_URL = "http://{{ omnivoip_ip }}:8090/Omnisup/index.php?page=Lista_Campanas&supervId="
+OML_SUPERVISION_URL = "https://{{ omnivoip_ip }}:8443/Omnisup/index.php?page=Lista_Campanas&supervId="
 
 
 # 'OML_KAMAILIO_IP': ip donde se encuentra kamailio
