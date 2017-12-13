7fcf4f40ed9d11d2fda3031c3e39ffd5b7c29a4f
freetech <desarrollo@freetechsolutions.com.ar>
2017-12-13 15:22:02 -0300

    cambio del script de deploy para automatizar aun mas

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -9,6 +9,26 @@
 
 echo "Bienvenido al asistente de instalación de Omnileads"
 echo ""
+echo "Pasos preliminares:"
+echo "Instalando pip, virtualenv y git"
+apt-get -y install python-pip git virtualenv
+echo ""
+echo "Instalando ansible 2.4.0"
+pip install 'ansible==2.4.0.0'
+echo "Generando llaves públicas de usuario actual"
+ssh-keygen
+echo "Clonando el repositorio de git de ominicontacto en ~ (no olvidar poner la llave publica en bitbucket"
+cd ~
+git clone git@bitbucket.org:freetechdesarrollo/ominicontacto.git
+cd ominicontacto
+git config --global user.name "freetech"
+git config --global user.email "desarrollo@freetechsolutions.com.ar"
+git fetch
+git checkout develop
+rm -rf /etc/ansible
+echo "Copiando la carpeta ansible a /etc/"
+cp -r ~/ominicontacto/ansible /etc/
+
 echo "Ingrese 1 si es post-install o 2 si es un fresh install"
 echo -en "Opcion: "; read type_install
 echo "Ingrese 1 si va instalar en Debian, 2 si va a instalar en SangomaOS o 3 si va a instalar en Centos 7"
@@ -24,8 +44,27 @@ else
     echo  ""
 fi
 
+echo "Parámetros de la aplicación"
+echo -en "Ingrese valor de variable session_cookie_age: "; read session_cookie
+sed -i "s/\(^session_\).*/session_cookie_age: $session_cookie/" /etc/ansible/group_vars/all
+echo -en "Ingrese la contraseña de superuser de Omnileads: "; read admin_pass
+sed -i "s/\(^admin_pass\).*/admin_pass: $admin_pass/" /etc/ansible/group_vars/all
 
 if [ $opcion -eq 1 ]; then
+    echo -en "Ingrese IP  de omni-voip: "; read omnivoip_ip
+    sed -i "s/\(^omnivoip_ip:\).*/omnivoip_ip: $omnivoip_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^192.168.70.62\).*/$omnivoip_ip/" /etc/ansible/hosts
+
+    echo -en "Ingrese IP  de omni-app: "; read omniapp_ip
+    sed -i "s/\(^omniapp_ip:\).*/omniapp_ip: $omniapp_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^192.168.70.63\).*/$omniapp_ip/" /etc/ansible/hosts
+
+    echo -en "Ingrese fqdn  de omni-voip: "; read omnivoip_fqdn
+    sed -i "s/\(^omnivoip_fqdn:\).*/omnivoip_ip: $omnivoip_fqdn/" /etc/ansible/group_vars/all
+
+    echo -en "Ingrese fqdn  de omni-app: "; read omniapp_fqdn
+    sed -i "s/\(^omniapp_fqdn:\).*/omniapp_fqdn: $omniapp_fqdn/" /etc/ansible/group_vars/all
+
     echo "Ejecutando Ansible en Debian omni-voip"
     ansible-playbook -s /etc/ansible/omnivoip/omni-voip-debian.yml -u root
     ResultadoAnsible=`echo $?`
@@ -34,6 +73,16 @@ if [ $opcion -eq 1 ]; then
     echo ""
 
 elif [ $opcion -eq 2 ]; then
+    echo -en "Ingrese IP  de omni-freepbx: "; read omnifreepbx_ip
+    sed -i "s/\(^omnifreepbx_ip:\).*/omnifreepbx_ip: $omnifreepbx_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^192.168.99.2\).*/$omnifreepbx_ip/" /etc/ansible/hosts
+
+    echo -en "Ingrese fqdn  de omni-freepbx: "; read omnifreepbx_fqdn
+    sed -i "s/\(^omnicentos_fqdn:\).*/omnicentos_fqdn: $omnifreepbx_fqdn/" /etc/ansible/group_vars/all
+
+    echo "Transifiendo llave publica a usuario root de SangomaOS"
+    ssh-copy-id -i ~/.ssh/id_rsa.pub root@$omnifreepbx_ip
+
     echo "Ejecutando Ansible en SangomaOS"
     ansible-playbook -s /etc/ansible/omnivoip/omni-voip-freepbx.yml -u root
     ResultadoAnsible=`echo $?`
@@ -42,6 +91,16 @@ elif [ $opcion -eq 2 ]; then
     echo ""
 
 elif [ $opcion -eq 3 ]; then
+    echo -en "Ingrese IP  de omni-centos: "; read omnicentos_ip
+    sed -i "s/\(^omnifcentos_ip:\).*/omnicentos_ip: $omnicentos_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^172.16.20.12\).*/$omnicentos_ip/" /etc/ansible/hosts
+
+    echo -en "Ingrese fqdn  de omni-freepbx: "; read omnicentos_fqdn
+    sed -i "s/\(^omnicentos_fqdn:\).*/omnicentos_fqdn: $omnicentos_fqdn/" /etc/ansible/group_vars/all
+
+    echo "Transifiendo llave publica a usuario root de SangomaOS"
+    ssh-copy-id -i ~/.ssh/id_rsa.pub root@$omnicentos_ip
+
     echo "Ejecutando Ansible en Centos"
     ansible-playbook -s /etc/ansible/omnivoip/omni-voip-centos.yml -u root
     ResultadoAnsible=`echo $?`
