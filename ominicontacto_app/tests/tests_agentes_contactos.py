8124d88ff01d12071216372f49adeac406123dab
Simon <simon.moreno@freetechsolutions.com.ar>
2017-12-21 14:31:12 -0300

    Lista de contactos de agentes: Refactor de tests.

--- ominicontacto_app/tests/tests_agentes_contactos.py
+++ ominicontacto_app/tests/tests_agentes_contactos.py
@@ -20,50 +20,45 @@ class AgentesContactosTests(OMLBaseTest):
     PWD = u'admin123'
 
     def setUp(self):
+        """
+        Creo dos agentes con una campaña con un contacto cada una.
+        """
+        self.usuario_agente, self.campana_propia, self.contacto_propio = \
+            self._crear_agente_con_campana_y_contacto()
+        
+        self.usuario_agente_2, self.campana_ajena, self.contacto_ajeno = \
+            self._crear_agente_con_campana_y_contacto()
+        
+    def _crear_agente_con_campana_y_contacto(self):
+        usuario = self._crear_agente()
+        campana = CampanaFactory.create(tiempo_desconexion=3)
+        self._hacer_miembro(usuario, campana)
+        contacto = ContactoFactory.create(bd_contacto=campana.bd_contacto)
+        return usuario, campana, contacto
 
-        self.usuario_agente = UserFactory(is_staff=False, is_supervisor=False)
-        self.usuario_agente.set_password(self.PWD)
-        self.usuario_agente.save()
-        self.agente_profile = AgenteProfileFactory.create(user=self.usuario_agente)
+    def _crear_agente(self):
+        usuario_agente = UserFactory(is_staff=False, is_supervisor=False)
+        usuario_agente.set_password(self.PWD)
+        usuario_agente.save()
+        agente_profile = AgenteProfileFactory.create(user=usuario_agente)
+        return usuario_agente
 
-        self.tiempo_desconexion = 3
-        self.campana = CampanaFactory.create()
-        self.campana_activa_propia = CampanaFactory.create(
-            estado=Campana.ESTADO_ACTIVA, type=Campana.TYPE_PREVIEW,
-            tiempo_desconexion=self.tiempo_desconexion)
-
-        self.contacto = ContactoFactory.create(bd_contacto=self.campana_activa_propia.bd_contacto)
-        self.campana_activa_propia.bd_contacto.contactos.add(self.contacto)
-        self.queue = QueueFactory.create(campana=self.campana_activa_propia)
-        self.queue_member = QueueMemberFactory.create(member=self.agente_profile, 
-            queue_name=self.queue)
-
-
-        self.usuario_agente_2 = UserFactory(is_staff=False, is_supervisor=False)
-        self.usuario_agente_2.set_password(self.PWD)
-        self.usuario_agente_2.save()
-        self.agente_profile_2 = AgenteProfileFactory.create(user=self.usuario_agente_2)
-
-        self.campana_activa_ajena = CampanaFactory.create(
-            estado=Campana.ESTADO_ACTIVA, type=Campana.TYPE_PREVIEW,
-            tiempo_desconexion=self.tiempo_desconexion)
-        self.contacto_ajeno = ContactoFactory.create(
-        	bd_contacto=self.campana_activa_ajena.bd_contacto)
-        self.queue_ajena = QueueFactory.create(campana=self.campana_activa_ajena)
-        self.queue_member_ajeno = QueueMemberFactory.create(member=self.agente_profile_2, 
-			queue_name=self.queue_ajena)
-
+    def _hacer_miembro(self, usuario_agente, campana):
+        agente = usuario_agente.get_agente_profile()
+        queue = QueueFactory.create(campana=campana)
+        queue_member = QueueMemberFactory.create(member=agente, queue_name=queue)
 
     def test_agente_profile_contactos_de_campanas_de_las_que_es_miembro(self):
-    	contactos = self.agente_profile.get_contactos_de_campanas_miembro()
+        agente_profile = self.usuario_agente.get_agente_profile()
+        contactos = agente_profile.get_contactos_de_campanas_miembro()
 
-    	self.assertEqual(contactos.count(), 1)
-    	self.assertEqual(self.contacto, contactos[0])
+        self.assertEqual(contactos.count(), 1)
+        self.assertEqual(self.contacto_propio, contactos[0])
 
     def test_contacto_list_muestra_solo_contactos_de_campanas_de_las_que_es_miembro(self):
         self.client.login(username = self.usuario_agente.username, password=self.PWD)
         url = reverse('contacto_list', args=[1])
         response = self.client.get(url, follow=True)
 
-        self.assertContains(response, self.contacto.telefono)
+        self.assertContains(response, self.contacto_propio.telefono)
         self.assertNotContains(response, self.contacto_ajeno.telefono)
