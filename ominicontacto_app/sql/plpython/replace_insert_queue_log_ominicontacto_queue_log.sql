e0393f572a9a7a0d9b5e777323713eb36ec8550d
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2017-12-21 08:19:35 -0500

    - Escribe en el modelo Queuelog teniendo en cuenta la información   recibida de campañas manuales y preview

--- ominicontacto_app/sql/plpython/replace_insert_queue_log_ominicontacto_queue_log.sql
+++ ominicontacto_app/sql/plpython/replace_insert_queue_log_ominicontacto_queue_log.sql
@@ -1,5 +1,8 @@
 CREATE OR REPLACE FUNCTION insert_queue_log_ominicontacto_queue_log() returns trigger as $$import datetime
 from plpy import spiexceptions
+
+TYPE_ENTRANTE, TYPE_DIALER, TYPE_MANUAL, TYPE_PREVIEW = range(1, 5)
+
 tiempo = TD['new']['time']
 fecha = datetime.datetime.strptime(tiempo, '%Y-%m-%d %H:%M:%S.%f')
 callid = TD['new']['callid']
@@ -10,7 +13,13 @@ data1 = TD['new']['data1']
 data2 = TD['new']['data2']
 data3 = TD['new']['data3']
 data4 = TD['new']['data4']
-data5 = TD['new']['data5']
+
+if data4 = 'saliente':
+   data5 = TYPE_MANUAL
+elif data5 = 'preview':
+    data5 = TYPE_PREVIEW
+else:
+    data5 = TD['new']['data5']
 
 plan = plpy.prepare("INSERT INTO ominicontacto_app_queuelog(time, callid, queuename, agent, event, data1, data2, data3, data4, data5, campana_id, agent_id) VALUES($1 ,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
 ["timestamp with time zone", "text", "text", "text", "text", "text", "text", "text", "text", "text", "int", "int"])
