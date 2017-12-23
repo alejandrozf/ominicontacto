83cc945ce99a9cb558038c848c6867502dd7cade
federicopeker <federicopeker@gmail.com>
2017-12-23 18:58:04 +0200

    descomenta para update la conversion del audio

--- ominicontacto_app/views_archivo_de_audio.py
+++ ominicontacto_app/views_archivo_de_audio.py
@@ -90,45 +90,45 @@ class ArchivoAudioUpdateView(UpdateView):
     form_class = ArchivoDeAudioForm
 
 
-    # def form_valid(self, form):
-    #     self.object = form.save()
-    #
-    #     if self.request.FILES.get('audio_original'):
-    #         try:
-    #             conversor_audio = ConversorDeAudioService()
-    #             conversor_audio.convertir_audio_de_archivo_de_audio_globales(
-    #                 self.object)
-    #             return redirect(self.get_success_url())
-    #         except FtsAudioConversionError:
-    #             self.object.audio_original = None
-    #             self.object.save()
-    #
-    #             message = '<strong>Operación Errónea!</strong> \
-    #                 Hubo un inconveniente en la conversión del audio. Por favor \
-    #                 verifique que el archivo subido sea el indicado.'
-    #             messages.add_message(
-    #                 self.request,
-    #                 messages.ERROR,
-    #                 message,
-    #             )
-    #             return self.form_invalid(form)
-    #         except Exception, e:
-    #             self.object.audio_original = None
-    #             self.object.save()
-    #
-    #             logger.warn("convertir_audio_de_archivo_de_audio_globales(): "
-    #                         "produjo un error inesperado. Detalle: %s", e)
-    #
-    #             message = '<strong>Operación Errónea!</strong> \
-    #                 Se produjo un error inesperado en la conversión del audio.'
-    #             messages.add_message(
-    #                 self.request,
-    #                 messages.ERROR,
-    #                 message,
-    #             )
-    #             return self.form_invalid(form)
-    #
-    #     return redirect(self.get_success_url())
+    def form_valid(self, form):
+        self.object = form.save()
+
+        if self.request.FILES.get('audio_original'):
+            try:
+                conversor_audio = ConversorDeAudioService()
+                conversor_audio.convertir_audio_de_archivo_de_audio_globales(
+                    self.object)
+                return redirect(self.get_success_url())
+            except OmlAudioConversionError:
+                self.object.audio_original = None
+                self.object.save()
+
+                message = '<strong>Operación Errónea!</strong> \
+                    Hubo un inconveniente en la conversión del audio. Por favor \
+                    verifique que el archivo subido sea el indicado.'
+                messages.add_message(
+                    self.request,
+                    messages.ERROR,
+                    message,
+                )
+                return self.form_invalid(form)
+            except Exception, e:
+                self.object.audio_original = None
+                self.object.save()
+
+                logger.warn("convertir_audio_de_archivo_de_audio_globales(): "
+                            "produjo un error inesperado. Detalle: %s", e)
+
+                message = '<strong>Operación Errónea!</strong> \
+                    Se produjo un error inesperado en la conversión del audio.'
+                messages.add_message(
+                    self.request,
+                    messages.ERROR,
+                    message,
+                )
+                return self.form_invalid(form)
+
+        return redirect(self.get_success_url())
 
     def get_success_url(self):
         return reverse('lista_archivo_audio')
@@ -142,7 +142,6 @@ class ArchivoAudioDeleteView(DeleteView):
 
     model = ArchivoDeAudio
     template_name = 'archivo_audio/elimina_archivo_audio.html'
-    #queryset = ArchivoDeAudio.objects.all()
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
