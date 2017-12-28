9e30b0de3ece682c869a43e6ff5f2e7c5cd89504
Simon <simon.moreno@freetechsolutions.com.ar>
2017-12-28 12:59:50 -0300

    Campaña Dialer: no se pasa extension del audio para contestadores en config de asterisk

--- ominicontacto_app/asterisk_config.py
+++ ominicontacto_app/asterisk_config.py
@@ -126,7 +126,8 @@ class QueueDialplanConfigCreator(object):
         if campana.queue_campana.detectar_contestadores:
             if campana.queue_campana.audio_para_contestadores:
                 filepath = campana.queue_campana.audio_para_contestadores.audio_asterisk.path
-                param_generales['filename_audio_contestadores'] = os.path.basename(filepath)
+                filename = os.path.splitext(os.path.basename(filepath))[0]
+                param_generales['filename_audio_contestadores'] = filename
                 generador_queue = self._generador_dialer_factory. \
                     crear_generador_para_campana_dialer_contestadores_end_con_audio(param_generales)
             else:
