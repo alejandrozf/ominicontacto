608563ba60cee9c65529327e3fd5f78d52cf62c2
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-01-11 14:52:56 -0300

    - Fix al obtener contexto y refactor de tests de calificaciones

--- ominicontacto_app/tests/tests_calificacion.py
+++ ominicontacto_app/tests/tests_calificacion.py
@@ -37,12 +37,7 @@ class CalificacionTests(OMLBaseTest):
 
         self.client.login(username=self.usuario_agente, password=self.PWD)
 
-    def test_no_se_admite_tipo_calificacion_cliente_vacia_en_creacion_calificacion(self):
-        url = reverse('calificacion_formulario_create',
-                      kwargs={'id_agente': self.agente_profile.pk,
-                              'pk_campana': self.campana.pk,
-                              'pk_contacto': self.contacto.pk,
-                              'wombat_id': 0})
+    def _obtener_post_data_calificacion_cliente(self):
         post_data = {
             'telefono': self.contacto.telefono,
             'calificacioncliente_set-TOTAL_FORMS': 1,
@@ -60,8 +55,17 @@ class CalificacionTests(OMLBaseTest):
             'calificacioncliente_set-0-agente': self.agente_profile.pk,
             'calificacioncliente_set-0-agendado': False,
             'calificacioncliente_set-0-id': ''}
+        return post_data
+
+    def test_no_se_admite_tipo_calificacion_cliente_vacia_en_creacion_calificacion(self):
+        url = reverse('calificacion_formulario_create',
+                      kwargs={'id_agente': self.agente_profile.pk,
+                              'pk_campana': self.campana.pk,
+                              'pk_contacto': self.contacto.pk,
+                              'wombat_id': 0})
+        post_data = self._obtener_post_data_calificacion_cliente()
         response = self.client.post(url, post_data, follow=True)
-        calificacion_form = response.context.get('calificacion_form')
+        calificacion_form = response.context_data.get('calificacion_form')
         self.assertFalse(calificacion_form.is_valid())
 
     def test_no_se_admite_tipo_calificacion_cliente_vacia_en_modificacion_calificacion(self):
@@ -72,26 +76,10 @@ class CalificacionTests(OMLBaseTest):
                               'pk_campana': self.campana.pk,
                               'pk_contacto': self.contacto.pk,
                               'wombat_id': 0})
-        post_data = {
-            'telefono': self.contacto.telefono,
-            'calificacioncliente_set-TOTAL_FORMS': 1,
-            'calificacioncliente_set-INITIAL_FORMS': 0,
-            'calificacioncliente_set-MIN_NUM_FORMS': 0,
-            'calificacioncliente_set-MAX_NUM_FORMS': 1,
-            'calificacioncliente_set-TOTAL_FORMS': 1,
-            'calificacioncliente_set-INITIAL_FORMS': 0,
-            'calificacioncliente_set-MIN_NUM_FORMS': 0,
-            'calificacioncliente_set-MAX_NUM_FORMS': 1,
-            'calificacioncliente_set-0-calificacion': '',
-            'calificacioncliente_set-0-campana': self.campana.pk,
-            'calificacioncliente_set-0-contacto': self.contacto.pk,
-            'calificacioncliente_set-0-es_venta': False,
-            'calificacioncliente_set-0-agente': self.agente_profile.pk,
-            'calificacioncliente_set-0-agendado': False,
-            'calificacioncliente_set-0-id': calificacion_cliente.pk
-        }
+        post_data = self._obtener_post_data_calificacion_cliente()
+        post_data['calificacioncliente_set-0-id'] = calificacion_cliente.pk
         response = self.client.post(url, post_data, follow=True)
-        calificacion_form = response.context.get('calificacion_form')
+        calificacion_form = response.context_data.get('calificacion_form')
         self.assertFalse(calificacion_form.is_valid())
 
     @patch('requests.post')
@@ -101,22 +89,7 @@ class CalificacionTests(OMLBaseTest):
                               'pk_campana': self.campana.pk,
                               'pk_contacto': self.contacto.pk,
                               'wombat_id': 0})
-        post_data = {
-            'telefono': self.contacto.telefono,
-            'calificacioncliente_set-TOTAL_FORMS': 1,
-            'calificacioncliente_set-INITIAL_FORMS': 0,
-            'calificacioncliente_set-MIN_NUM_FORMS': 0,
-            'calificacioncliente_set-MAX_NUM_FORMS': 1,
-            'calificacioncliente_set-TOTAL_FORMS': 1,
-            'calificacioncliente_set-INITIAL_FORMS': 0,
-            'calificacioncliente_set-MIN_NUM_FORMS': 0,
-            'calificacioncliente_set-MAX_NUM_FORMS': 1,
-            'calificacioncliente_set-0-calificacion': self.calificacion_gestion.pk,
-            'calificacioncliente_set-0-campana': self.campana.pk,
-            'calificacioncliente_set-0-contacto': self.contacto.pk,
-            'calificacioncliente_set-0-es_venta': False,
-            'calificacioncliente_set-0-agente': self.agente_profile.pk,
-            'calificacioncliente_set-0-agendado': False,
-            'calificacioncliente_set-0-id': ''}
+        post_data = self._obtener_post_data_calificacion_cliente()
+        post_data['calificacioncliente_set-0-calificacion'] = self.calificacion_gestion.pk,
         response = self.client.post(url, post_data, follow=True)
         self.assertTemplateUsed(response, 'formulario/formulario_create.html')
