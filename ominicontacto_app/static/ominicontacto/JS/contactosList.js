27edbedea6dcb58fe13c57571caa51649d110377
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-01-17 15:25:35 -0300

    - Se adicionan datos al template y se crean bindings para realizar el click2call

--- ominicontacto_app/static/ominicontacto/JS/contactosList.js
+++ ominicontacto_app/static/ominicontacto/JS/contactosList.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('#agenteContactosTable').DataTable( {
     // Convierte a datatable la tabla de contactos
     language: {
@@ -12,5 +13,16 @@ $(document).ready(function(){
       lengthMenu: "Mostrar _MENU_ entradas",
       info: "Mostrando _START_ a _END_ de _TOTAL_ entradas",
     }
-  } );
+  });
+
+  var $input_contacto = $('#pk_contacto');
+
+  $('.contacto-lista').each(function() {
+    $(this).on('click', function() {
+      var $button = $(this);
+      var pk_contacto = $button.data('contacto');
+      $input_contacto.attr('value', pk_contacto);
+      $('#lista_llamar_contacto').trigger('click');
+    });
+  });
 });
