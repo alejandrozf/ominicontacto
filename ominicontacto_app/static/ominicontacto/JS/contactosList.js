9c882e2d869c642b44984302242af22ea873fd21
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-01-19 13:46:54 -0300

    - Agrega botones con acciones en el momento de creación de cada fila

--- ominicontacto_app/static/ominicontacto/JS/contactosList.js
+++ ominicontacto_app/static/ominicontacto/JS/contactosList.js
@@ -14,10 +14,55 @@ $(document).ready(function(){
     });
   }
 
+  function create_node (type, attrs) {
+    var $node = $("<" + type + "/>", attrs);
+    return $node;
+  }
+
   $('#agenteContactosTable')
     .on( 'draw.dt', function () { conectar_contactos_llamadas();})
     .DataTable( {
       // Convierte a datatable la tabla de contactos
+      "createdRow": function ( row, data, index ) {
+        var pk_contacto = data[0];
+
+        var $modificar_contacto = create_node('a', {
+          'href': '/contacto/'+ pk_contacto + '/update/$',
+        });
+        var $span_modificar_contacto = create_node('span', {
+          'class': 'glyphicon glyphicon-tasks',
+          'aria-hidden': 'true',
+          'title': 'Modificar',
+        });
+        $modificar_contacto.append($span_modificar_contacto);
+
+        var $llamar_contacto = create_node('button', {
+          'class': 'contacto-lista btn btn-link',
+        });
+        var $span_llamar_contacto = create_node('span', {
+          'class': 'glyphicon glyphicon-earphone',
+          'aria-hidden': 'true',
+          'title': 'Llamar',
+        });
+        $llamar_contacto.append($span_llamar_contacto);
+        $llamar_contacto
+          .on('click', function() {
+            $inputContacto.attr('value', pk_contacto);
+            $('#lista_llamar_contacto').trigger('click');
+          });
+
+        var $mailto_contacto = create_node('a', {
+          'href': '#',
+        });
+        var $span_mailto_contacto = create_node('span', {
+          'class': 'glyphicon glyphicon-envelope',
+          'aria-hidden': 'true',
+          'title': 'Email',
+        });
+        $mailto_contacto.append($span_mailto_contacto);
+
+        $(row).find('td').last().append([$modificar_contacto, $llamar_contacto, $mailto_contacto]);
+      },
       serverSide: true,
       processing: true,
       ajax: '/api/campana/' + pk_campana + '/contactos/',
