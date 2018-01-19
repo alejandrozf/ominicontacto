a889eb44a13659d92b6582267b22e954371e915c
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-01-19 14:39:08 -0300

    - Se realiza refactor de código JS

--- ominicontacto_app/static/ominicontacto/JS/contactosList.js
+++ ominicontacto_app/static/ominicontacto/JS/contactosList.js
@@ -8,48 +8,53 @@ $(document).ready(function(){
     return $node;
   }
 
-  $('#agenteContactosTable')
-    .DataTable( {
-      // Convierte a datatable la tabla de contactos
-      "createdRow": function ( row, data, index ) {
-        var pk_contacto = data[0];
+  function obtener_nodos_acciones (row, data, index) {
+    var pk_contacto = data[0];
 
-        var $modificar_contacto = create_node('a', {
-          'href': '/contacto/'+ pk_contacto + '/update/$',
+    var $modificar_contacto = create_node('a', {
+          'href': '/contacto/'+ pk_contacto + '/update/',
         });
         var $span_modificar_contacto = create_node('span', {
           'class': 'glyphicon glyphicon-tasks',
           'aria-hidden': 'true',
           'title': 'Modificar',
         });
-        $modificar_contacto.append($span_modificar_contacto);
+    $modificar_contacto.append($span_modificar_contacto);
 
-        var $llamar_contacto = create_node('button', {
+    var $llamar_contacto = create_node('button', {
           'class': 'contacto-lista btn btn-link',
         });
-        var $span_llamar_contacto = create_node('span', {
+    var $span_llamar_contacto = create_node('span', {
           'class': 'glyphicon glyphicon-earphone',
           'aria-hidden': 'true',
           'title': 'Llamar',
         });
-        $llamar_contacto.append($span_llamar_contacto);
-        $llamar_contacto
+    $llamar_contacto.append($span_llamar_contacto);
+    $llamar_contacto
           .on('click', function() {
             $inputContacto.attr('value', pk_contacto);
             $('#lista_llamar_contacto').trigger('click');
           });
 
-        var $mailto_contacto = create_node('a', {
+    var $mailto_contacto = create_node('a', {
           'href': '#',
         });
-        var $span_mailto_contacto = create_node('span', {
+    var $span_mailto_contacto = create_node('span', {
           'class': 'glyphicon glyphicon-envelope',
           'aria-hidden': 'true',
           'title': 'Email',
         });
-        $mailto_contacto.append($span_mailto_contacto);
+    $mailto_contacto.append($span_mailto_contacto);
 
-        $(row).find('td').last().append([$modificar_contacto, $llamar_contacto, $mailto_contacto]);
+    return [$modificar_contacto, $llamar_contacto, $mailto_contacto];
+  }
+
+  $('#agenteContactosTable')
+    .DataTable( {
+      // Convierte a datatable la tabla de contactos
+      "createdRow": function ( row, data, index ) {
+        nodo_acciones = obtener_nodos_acciones(row, data, index);
+        $(row).find('td').last().append(nodo_acciones);
       },
       serverSide: true,
       processing: true,
