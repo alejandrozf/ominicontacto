97e4999eb7e26b2a11ec7e39518c1cd5533f5d3a
Federico Peker <federicopeker@gmail.com>
2018-02-19 19:43:11 -0300

    cambia la forma de calcular sip extension en el agente

--- ominicontacto_app/views.py
+++ ominicontacto_app/views.py
@@ -211,8 +211,7 @@ class AgenteProfileCreateView(CreateView):
         self.object = form.save(commit=False)
         usuario = User.objects.get(pk=self.kwargs['pk_user'])
         self.object.user = usuario
-        self.object.sip_extension = AgenteProfile.objects.\
-            obtener_ultimo_sip_extension()
+        self.object.sip_extension = 1000 + usuario.id
         # generar un sip_password aleatorio
         self.object.sip_password = User.objects.make_random_password()
         self.object.reported_by = self.request.user
