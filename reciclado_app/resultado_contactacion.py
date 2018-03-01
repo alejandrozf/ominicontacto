4b63e7619621ef5bb8142909a3b3a17d7203956e
Federico Peker <federicopeker@gmail.com>
2018-03-01 11:20:51 -0300

    modifica metodo para obtener los no contactados

--- reciclado_app/resultado_contactacion.py
+++ reciclado_app/resultado_contactacion.py
@@ -28,9 +28,8 @@ class EstadisticasContactacion():
         campana_log_wombat = campana_log_wombat.values(
             'estado', 'calificacion').annotate(Count('estado'))
 
-        resultado_nombre = []
-        resultado_cantidad = []
-        total_no_atendidos = 0
+        count_estados = {}
+
         for resultado in campana_log_wombat:
             estado = resultado['estado']
             if estado == "RS_LOST" and resultado['calificacion'] == "":
@@ -45,11 +44,10 @@ class EstadisticasContactacion():
                 estado = "Error de sistema"
             elif estado == "RS_REJECTED":
                 estado = "Congestion"
-            resultado_nombre.append(estado)
-            resultado_cantidad.append(resultado['estado__count'])
-            total_no_atendidos += resultado['estado__count']
-        resultado_nombre.append("Contestador Detectado")
-        cantidad_constestador = campana_log_terminated.count()
-        resultado_cantidad.append(cantidad_constestador)
-        total_no_atendidos += cantidad_constestador
-        return resultado_nombre, resultado_cantidad, total_no_atendidos
+            if estado in count_estados.keys():
+                count_estados[estado] += resultado['estado__count']
+            else:
+                count_estados.update({estado: resultado['estado__count']})
+        if campana_log_terminated.count() > 0:
+            count_estados.update({'Contestador Detectado': campana_log_terminated.count()})
+        return count_estados
