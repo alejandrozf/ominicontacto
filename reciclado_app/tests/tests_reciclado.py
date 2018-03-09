b30ac29684d53f7b2d24a0f5d72212c817921d16
Federico Peker <federicopeker@gmail.com>
2018-03-09 10:52:29 -0300

    agrega assert para los estados no contactados

--- reciclado_app/tests/tests_reciclado.py
+++ reciclado_app/tests/tests_reciclado.py
@@ -14,15 +14,6 @@ from reciclado_app.resultado_contactacion import EstadisticasContactacion
 
 
 class RecicladoTest(OMLBaseTest):
-    MAP_LOG_WOMBAT = {
-        "RS_LOST": 1,
-        "RS_BUSY": 2,
-        "RS_NOANSWER": 3,
-        "RS_NUMBER": 4,
-        "RS_ERROR": 5,
-        "RS_REJECTED": 6,
-        "CONTESTADOR": 7,
-    }
 
     def setUp(self):
         base_datos = self.crear_base_datos_contacto(cant_contactos=100)
@@ -32,6 +23,10 @@ class RecicladoTest(OMLBaseTest):
         self.agente = self.crear_agente_profile(user_agente)
 
     def test_devuelve_correctamente_no_contactacion(self):
+        """
+        este test testea todos los resultados las cantidad de los no
+        contactdos chequeando que devuelva correctamente
+        """
 
         # estados no contactados:
         estados = ["RS_LOST", "RS_BUSY", "RS_NOANSWER", "RS_NUMBER",
@@ -57,10 +52,37 @@ class RecicladoTest(OMLBaseTest):
         campana_log_wombat = self.campana.logswombat.filter(estado__in=estados)
         campana_log_wombat = campana_log_wombat.values(
             'estado', 'calificacion').annotate(Count('estado'))
-        print campana_log_wombat
-        print no_contactados
-        print no_contactados_lista
-        # for no_contacto in no_contactados_choice:
-        #
-        #     self.assertEquals()
 
+        for log in campana_log_wombat:
+            estado = log['estado']
+            if estado == "RS_LOST" and log['calificacion'] == "":
+                id_estado = EstadisticasContactacion.AGENTE_NO_DISPONIBLE
+                self.assertEquals(no_contactados_lista[id_estado], log['estado__count'])
+            elif estado == "RS_BUSY":
+                id_estado = EstadisticasContactacion.OCUPADO
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "RS_NOANSWER":
+                id_estado = EstadisticasContactacion.NO_CONTESTA
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "RS_NUMBER":
+                id_estado = EstadisticasContactacion.NUMERO_ERRONEO
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "RS_ERROR":
+                id_estado = EstadisticasContactacion.ERROR_DE_SISTEMA
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "RS_REJECTED":
+                id_estado = EstadisticasContactacion.CONGESTION
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "TERMINATED" and log['calificacion'] == "CONTESTADOR":
+                id_estado = EstadisticasContactacion.CONTESTADOR
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
+            elif estado == "TERMINATED" and log['calificacion'] == "":
+                id_estado = EstadisticasContactacion.AGENTE_NO_CALIFICO
+                self.assertEquals(no_contactados_lista[id_estado],
+                                  log['estado__count'])
