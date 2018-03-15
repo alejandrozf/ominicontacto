bcd30a639cce82d66a80f17a679b85833d49055c
lionite <felipe.macias@freetechsolutions.com.ar>
2018-03-15 15:46:32 -0300

    hago el cambio para poder ejecutar ansible en el repo sin problema

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -20,13 +20,13 @@ USAGE="
             -t: ingresar ip, opcion de SO, tags de ansible (TAREAS A EJECUTAR y no ejecutar) \n
         \n
        EJEMPLOS: \n
-       - ./deploy.sh -r develop -t all -> deployará la rama develop, ejecutara todas las tareas \n
+       - ./deploy.sh -r develop -t hosts -> deployará la rama develop, ejecutara todas las tareas \n
        - ./deploy.sh -r release-0.4 -i -t kamailio,nginx,kamailio-cert -> deploya la rama release-0.4, pide datos del server, ejecuta las tareas de instalación de kamailio y de nginx  exceptuando la creacion de certificados (tiene que estar separado por coma) \n
        - ./deploy.sh -r release-0.4 -i -t asterisk,,kamailio-cert -> igual al anterior, solamente ejecutará tareas de instalación de asterisk exceptuando la creacion de certificados \n
        \n
        Tags disponibles: \n
-       all: ejecuta todos los procesos \n
-       asterisk-install: compila e instala asterisk \n
+       hosts: ejecuta todos los procesos \n
+       asterisk-insthosts: compila e instala asterisk \n
        asterisk-config: realiza tareas de configuracion de asterisk con OML \n
        asternic: instala y configura asternic y scripts de grabaciones \n
        deploy: instala la aplicación, setea el virtualenv y el entorno de OML \n
@@ -40,7 +40,7 @@ USAGE="
        pre-sangoma: ejecuta los prerequisitos de SangomaOS (instalacion de paquetes tambien) \n
        pre-centos: ejecuta los prerequisitos de Centos (instalacion de paquetes tambien) \n
        postgresusers: crea la base de datos y usuarios postgres \n
-       postinstall: ejecuta tareas necesarias para un post-deploy \n
+       postinsthosts: ejecuta tareas necesarias para un post-deploy \n
        queuelog-trigger: ejecuta el trigger de queuelog \n
        sshkey-transfer: realiza la transferencia de la llave publica ssh entre usuarios freetech y root \n
        supervision: deploya la supervision \n "
@@ -51,6 +51,12 @@ USAGE="
 }
 
 Rama() {
+    cd $current_directory
+    echo "Creando directorio temporal de ansible"
+    mkdir -p /var/tmp/ansible
+    TMP_ANSIBLE='/var/tmp/ansible'
+    echo "Copiando el contenido de ansible del repositorio al directorio temporal"
+    cp -a $current_directory/ansible/* $TMP_ANSIBLE
     echo "Pasando al deploy de OmniAPP"
     set -e
     echo ""
@@ -60,7 +66,6 @@ Rama() {
     #echo "   Inventario: $INVENTORY"
     echo ""
 
-    cd $current_directory
     git checkout $1
     git pull origin +$1:$1
 
@@ -105,7 +110,7 @@ Rama() {
 
 OML_BRANCH="${branch_name}"
 OML_COMMIT="${commit}"
-OML_BUILD_DATE="$(env LC_ALL=C LC_TIME=C date)"
+OML_BUILD_DATE="$(env LC_hosts=C LC_TIME=C date)"
 OML_AUTHOR="${author}"
 
 if __name__ == '__main__':
@@ -129,7 +134,7 @@ Preliminar() {
     echo "Instalando ansible 2.4.0"
 
     if [ -z $ANSIBLE ]; then
-        $PIP install 'ansible==2.4.0.0'
+        $PIP insthosts 'ansible==2.4.0.0'
     else
         echo "Ya tiene instalado ansible"
     fi
@@ -143,19 +148,17 @@ Preliminar() {
 
     echo "Parámetros de la aplicación"
     echo -en "Ingrese valor de variable session_cookie_age: "; read session_cookie
-    sed -i "s/\(^session_\).*/session_cookie_age: $session_cookie/" $current_directory/group_vars/all
+    sed -i "s/\(^session_\).*/session_cookie_age=$session_cookie/" $TMP_ANSIBLE/hosts
     echo -en "Ingrese la contraseña de superuser de Omnileads: "; read admin_pass
-    sed -i "s/\(^admin_pass\).*/admin_pass: $admin_pass/" $current_directory/group_vars/all
-    sed -i "s|inventory.*$|inventory = $current_directory\/hosts|g" $current_directory/ansible.cfg
-        sed -i "s|roles_path.*$|roles_path = $current_directory\/roles\/|g" $current_directory/ansible.cfg
+    sed -i "s/\(^admin_pass\).*/admin_pass=$admin_pass/" $TMP_ANSIBLE/hosts
 }
 
 IngresarIP(){
 
     echo -en "Ingrese el formato de audio en el que quiere las grabaciones: "; read audio
-    sed -i "s/\(^MONITORFORMAT\).*/MONITORFORMAT = \'$audio\'/" $current_directory/deploy/roles/oml_server/templates/oml_settings_local.py
+#    sed -i "s/\(^MONITORFORMAT\).*/MONITORFORMAT = \'$audio\'/" $TMP_ANSIBLE/deploy/roles/oml_server/templates/oml_settings_local.py
     echo -en "Ingrese fqdn  de maquina a deployar: "; read omnicentos_fqdn
-    sed -i "s/\(^omnicentos_fqdn:\).*/omnicentos_fqdn: $omnicentos_fqdn/" $current_directory/group_vars/all
+    sed -i "s/\(^omnicentos_fqdn=\).*/omnicentos_fqdn=$omnicentos_fqdn/" $TMP_ANSIBLE/hosts
     echo "Transifiendo llave publica a usuario root de Centos"
 }
 
@@ -165,7 +168,7 @@ Desarrollo() {
     echo "Escogió la opción -d por lo que el deploy es para una máquina de desarrollo"
     echo "#############################################################################"
     echo ""
-    sed -i "s/\(^desarrollo:\).*/desarrollo: 1/" $current_directory/group_vars/all
+    sed -i "s/\(^desarrollo\).*/desarrollo=1/" $TMP_ANSIBLE/hosts
 }
 
 Tag() {
@@ -176,18 +179,18 @@ Tag() {
 
 if [ $opcion -eq 1 ]; then
     echo -en "Ingrese IP  de omni-voip: "; read omnivoip_ip
-    sed -i "s/\(^omnivoip_ip:\).*/omnivoip_ip: $omnivoip_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^omnivoip_ip:\).*/omnivoip_ip: $omnivoip_ip/" /etc/ansible/hosts
     sed -i "s/\(^192.168.70.62\).*/$omnivoip_ip/" /etc/ansible/hosts.yml
 
     echo -en "Ingrese IP  de omni-app: "; read omniapp_ip
-    sed -i "s/\(^omniapp_ip:\).*/omniapp_ip: $omniapp_ip/" /etc/ansible/group_vars/all
+    sed -i "s/\(^omniapp_ip:\).*/omniapp_ip: $omniapp_ip/" /etc/ansible/hosts
     sed -i "s/\(^192.168.70.63\).*/$omniapp_ip/" /etc/ansible/hosts.yml
 
     echo -en "Ingrese fqdn  de omni-voip: "; read omnivoip_fqdn
-    sed -i "s/\(^omnivoip_fqdn:\).*/omnivoip_ip: $omnivoip_fqdn/" /etc/ansible/group_vars/all
+    sed -i "s/\(^omnivoip_fqdn:\).*/omnivoip_ip: $omnivoip_fqdn/" /etc/ansible/hosts
 
     echo -en "Ingrese fqdn  de omni-app: "; read omniapp_fqdn
-    sed -i "s/\(^omniapp_fqdn:\).*/omniapp_fqdn: $omniapp_fqdn/" /etc/ansible/group_vars/all
+    sed -i "s/\(^omniapp_fqdn:\).*/omniapp_fqdn: $omniapp_fqdn/" /etc/ansible/hosts
 
     echo "Ejecutando Ansible en Debian"
     ansible-playbook -s /etc/ansible/omnivoip/omni-voip-debian.yml -u root
@@ -198,18 +201,18 @@ if [ $opcion -eq 1 ]; then
 
 elif [ $opcion -eq 2 ]; then
 
-    sed -i "23s/.*/$ip ansible_ssh_port=22/" $current_directory/hosts
+    sed -i "8s/.*/$ip ansible_ssh_port=22/" $TMP_ANSIBLE/hosts
     echo "Ejecutando Ansible en SangomaOS"
-    ansible-playbook -s $current_directory/deploy/omnileads-freepbx.yml -u root --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}"
+    ansible-playbook -s $TMP_ANSIBLE/deploy/omnileads-freepbx.yml -u root --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}"
     ResultadoAnsible=`echo $?`
     echo "Finalizó la instalación omnileads"
     echo ""
 
 elif [ $opcion -eq 3 ]; then
 
-    sed -i "21s/.*/$ip ansible_ssh_port=22/" $current_directory/hosts
+    sed -i "6s/.*/$ip ansible_ssh_port=22/" $TMP_ANSIBLE/hosts
     echo "Ejecutando Ansible en Centos"
-    ansible-playbook -s $current_directory/deploy/omnileads-centos.yml -u root --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}"
+    ansible-playbook -s $TMP_ANSIBLE/deploy/omnileads-centos.yml -u root --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}"
     ResultadoAnsible=`echo $?`
     echo "Finalizó la instalación omnileads"
     echo ""
@@ -220,7 +223,7 @@ else
 fi
 
 if [ ${ResultadoAnsible} -ne 0 ];then
-    echo "Falló la ejecucion de Ansible, favor volver a correr el script"
+    echo "Fhostsó la ejecucion de Ansible, favor volver a correr el script"
     exit 0
 #else
 
