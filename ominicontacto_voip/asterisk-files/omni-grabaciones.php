faff890f15d90cf4e885ace2af44a2581dd3a629
Simon <simon.moreno@freetechsolutions.com.ar>
2018-04-09 17:07:47 -0300

    Actualiza agi grabaciones agregando campo duracion en 0

--- ominicontacto_voip/asterisk-files/omni-grabaciones.php
+++ ominicontacto_voip/asterisk-files/omni-grabaciones.php
@@ -14,12 +14,13 @@ $sip_agente=$argv[4];
 $campana=$argv[6];
 $fecha=$argv[7];
 $uid=$argv[8];
+$duracion=0; //$argv[9];
 //$fecha=date('Y-m-d H:m:s');
 
 $connection=pg_connect("host=127.0.0.1 port=5432 password=kamailiorw user=kamailio")
 or die('NO HAY CONEXION: ' . pg_last_error()); 
 
-$query ="INSERT INTO ominicontacto_app_grabacion (fecha,tipo_llamada,id_cliente,tel_cliente,grabacion,sip_agente,campana_id,uid) VALUES ('$fecha','$tipo_llamada','$id_cliente','$tel_cliente','$grabacion','$sip_agente','$campana','$uid');"; 
+$query ="INSERT INTO ominicontacto_app_grabacion (fecha,tipo_llamada,id_cliente,tel_cliente,grabacion,sip_agente,campana_id,uid,duracion) VALUES ('$fecha','$tipo_llamada','$id_cliente','$tel_cliente','$grabacion','$sip_agente','$campana','$uid','$duracion');"; 
 
 echo"$query\n";
 
