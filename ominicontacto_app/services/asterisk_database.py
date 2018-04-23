079d41e458a2858d2a4e0818357379e1e89ff825
Federico Peker <federicopeker@gmail.com>
2018-04-23 14:58:15 -0300

    separa en if separados las referencias

--- ominicontacto_app/services/asterisk_database.py
+++ ominicontacto_app/services/asterisk_database.py
@@ -14,18 +14,26 @@ class CampanaFamily(object):
             'TYPE': campana.type,
             'REC': campana.queue_campana.auto_grabacion,
             'AMD': campana.queue_campana.detectar_contestadores,
-            'AMDPLAY': "oml/{0}".format(
-                campana.queue_campana.audio_para_contestadores.get_filename_audio_asterisk()),
-            'WELCOMEPLAY': "olc/{0}".format(
-                campana.queue_campana.audio_de_ingreso.get_filename_audio_asterisk()),
             'CALLAGENTACTION': campana.tipo_interaccion,
-            'IDFORM': campana.formulario.pk,
-            'IDEXTERNALURL': campana.sitio_externo.pk,
             'RINGTIME': campana.queue_campana.timeout,
             'QUEUETIME': campana.queue_campana.wait,
             'MAXQCALLS': campana.queue_campana.maxlen,
             'SL': campana.queue_campana.servicelevel,
         }
+
+        if campana.queue_campana.audio_para_contestadores:
+            dict_campana.update({'AMDPLAY': "oml/{0}".format(
+                    campana.queue_campana.audio_para_contestadores.get_filename_audio_asterisk())})
+
+        if campana.queue_campana.audio_de_ingreso:
+            dict_campana.update({'WELCOMEPLAY': "olc/{0}".format(
+                campana.queue_campana.audio_de_ingreso.get_filename_audio_asterisk())})
+
+        if campana.formulario:
+            dict_campana.update({'IDFORM': campana.formulario.pk})
+        elif campana.sitio_externo:
+            dict_campana.update({'IDEXTERNALURL': campana.sitio_externo.pk})
+
         return dict_campana
 
     def create_dict(self, campana):
