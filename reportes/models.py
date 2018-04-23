76b6bd92d7c0cd6b5368613a866835b27886b48c
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-04-23 12:10:23 -0300

    - Se elimina campo queuename en modelo de logs de llamadas - Se agregan métodos __unicode__ a los nuevos modelos de logs - Se modifica migración inicial

--- reportes/models.py
+++ reportes/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
@@ -9,7 +11,6 @@ class LlamadasLog(models.Model):
     """
     time = models.DateTimeField(db_index=True)
     callid = models.CharField(max_length=32, blank=True, null=True)
-    queuename = models.CharField(max_length=32, blank=True, null=True)
     campana_id = models.IntegerField(db_index=True, blank=True, null=True)
     agente_id = models.IntegerField(db_index=True, blank=True, null=True)
     event = models.CharField(max_length=32, blank=True, null=True)
@@ -18,6 +19,11 @@ class LlamadasLog(models.Model):
     data3 = models.CharField(max_length=128, blank=True, null=True)
     data4 = models.CharField(max_length=128, blank=True, null=True)
     data5 = models.CharField(max_length=128, blank=True, null=True)
+
+    def __unicode__(self):
+        return "Log de llamada con fecha {0} con id de campaña {1} con id de agente {2} " \
+               "con el evento {3} ".format(self.time, self.campana_id,
+                                           self.agente_id, self.event)
 
 
 class ActividadAgenteLog(models.Model):
@@ -28,3 +34,8 @@ class ActividadAgenteLog(models.Model):
     agente_id = models.IntegerField(db_index=True, blank=True, null=True)
     event = models.CharField(max_length=32, blank=True, null=True)
     data1 = models.CharField(max_length=128, blank=True, null=True)
+
+    def __unicode__(self):
+        return "Log de actividad agente con fecha {0} para agente de id {1} con el evento {2} " \
+               "con data1 {3}".format(self.time, self.agente_id,
+                                      self.event, self.data1)
