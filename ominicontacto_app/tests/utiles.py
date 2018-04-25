a672545b8b134f912c7225f1d52295806c2af2af
Federico Peker <federicopeker@gmail.com>
2018-04-25 19:56:59 -0300

    modifica cambios para queue de entrante

--- ominicontacto_app/tests/utiles.py
+++ ominicontacto_app/tests/utiles.py
@@ -24,6 +24,7 @@ from ominicontacto_app.models import (
     ArchivoDeAudio
 )
 from ominicontacto_app.tests.factories import NombreCalificacionFactory
+from ominicontacto_app.services.audio_conversor import ConversorDeAudioService
 from mock import Mock
 
 
@@ -36,6 +37,7 @@ def rtel():
     """Devuelve nro telefonico aleatorio"""
     return unicode(random.randint(1140000000000000,
                                   1149999999999999))
+
 
 def _tmpdir():
     """Crea directorio temporal"""
@@ -354,6 +356,7 @@ class OMLTestUtilsMixin(object):
             ringinuse=True,
             setinterfacevar=True,
             queue_asterisk=Queue.objects.ultimo_queue_asterisk(),
+            audio_de_ingreso=self.crear_arhivo_de_audio()
         )
         queue.save()
 
@@ -444,6 +447,9 @@ class OMLTestUtilsMixin(object):
         archivo_de_audio = ArchivoDeAudio(id=1,
                                           descripcion="Audio",
                                           audio_original=original)
+        conversor_audio = ConversorDeAudioService()
+        conversor_audio.convertir_audio_de_archivo_de_audio_globales(
+            archivo_de_audio)
         archivo_de_audio.save = Mock()
         return archivo_de_audio
 
