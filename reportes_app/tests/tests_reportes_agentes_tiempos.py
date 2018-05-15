0ab8e87dc5f886c731d3e9fd42225d3abf772a8e
Federico Peker <federicopeker@gmail.com>
2018-05-15 17:10:14 -0300

    agrego modulo de tests para reporte de agentes de tiempo

---
+++ reportes_app/tests/tests_reportes_agentes_tiempos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests del modoulo 'reportes_app.reporte_agente_tiempos.py'
+"""
+
+from __future__ import unicode_literals
+
+from ominicontacto_app.tests.utiles import OMLBaseTest
+
+
+class ReportesAgenteTiemposTest(OMLBaseTest):
+
+    pass
