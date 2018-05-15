09abb81a0f9219c242e65ddf358895403067f376
Federico Peker <federicopeker@gmail.com>
2018-05-15 12:35:15 -0300

    elimina form, views, service viejos

--- ominicontacto_app/forms.py
+++ ominicontacto_app/forms.py
@@ -1134,29 +1134,6 @@ class CampanaDialerTemplateForm(forms.ModelForm):
         }
 
 
-class ReporteAgenteForm(forms.Form):
-    """
-    El form para reporte con fecha
-    """
-    fecha = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    agente = forms.MultipleChoiceField(required=False, choices=())
-    grupo_agente = forms.ChoiceField(required=False, choices=(), widget=forms.Select(
-        attrs={'class': 'form-control'}))
-    todos_agentes = forms.BooleanField(required=False)
-
-    def __init__(self, *args, **kwargs):
-        super(ReporteAgenteForm, self).__init__(*args, **kwargs)
-
-        agente_choice = [(agente.pk, agente.user.get_full_name())
-                         for agente in AgenteProfile.objects.filter(is_inactive=False)]
-        self.fields['agente'].choices = agente_choice
-        grupo_choice = [(grupo.id, grupo.nombre)
-                        for grupo in Grupo.objects.all()]
-        grupo_choice.insert(0, EMPTY_CHOICE)
-        self.fields['grupo_agente'].choices = grupo_choice
-
-
 class CampanaManualForm(CampanaMixinForm, forms.ModelForm):
     auto_grabacion = forms.BooleanField(required=False)
     detectar_contestadores = forms.BooleanField(required=False)
