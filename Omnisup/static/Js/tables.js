a08e874e4463e4e762dabdebc9fc9312afb45bfe
Pignatar0md <pignataromd@gmail.com>
2018-05-19 11:45:06 -0400

    agrega colores a los estados

--- Omnisup/static/Js/tables.js
+++ Omnisup/static/Js/tables.js
@@ -2,9 +2,9 @@ var tabagt;
 $(function () {
   tabagt = $('#tableAgt').DataTable({
     createdRow: function (row, data, dataIndex) {
-      if (data.estado === "Libre") {
+      if (data.estado === "READY") {
         $(row).css("background-color", "rgb(164, 235, 143)");
-      } else if (data.estado === "Llamada") {
+      } else if (data.estado === "ONCALL") {
         $(row).css("background-color", "rgb(44, 169, 231)");
       } else {//esta en pausa
         $(row).css("background-color", "rgb(249, 159, 157)");
