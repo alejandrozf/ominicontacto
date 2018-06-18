256de91883d5325c2782a8a466a1afaa1175a258
Federico Peker <federicopeker@gmail.com>
2018-06-18 15:18:40 -0300

    agrega servicio para regeneracion de rutas salientes en asterisk

---
+++ configuracion_telefonia_app/regeneracion_configuracion_tecnologia.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+"""
+Servicio de regenarción de la configuracion de telefonia
+"""
+
+from __future__ import unicode_literals
+
+import logging
+
+from ominicontacto_app.errors import OmlError
+from ominicontacto_app.asterisk_config import (
+    RutasSalientesConfigCreator, RutasSalientesConfigFile
+)
+from ominicontacto_app.services.asterisk_database import RutaSalienteFamily
+
+logger = logging.getLogger(__name__)
+
+
+class RestablecerConfiguracionTelefonicaError(OmlError):
+    """Indica que se produjo un error al crear regenerar archivos de asterisk ó insetar en asterisk."""
+    pass
+
+
+class SincronizadorDeConfiguracionDeRutaSalienteEnAstDB(object):
+
+    def __init__(self):
+        self.generador_rutas_en_astdb = RutaSalienteFamily()
+        self.generador_rutas_en_asterisk_conf = RutasSalientesConfigCreator()
+        self.config_rutas_file = RutasSalientesConfigFile()
+
+    def _generar_y_recargar_archivos_conf_asterisk(self):
+        proceso_ok = True
+        mensaje_error = ""
+
+        try:
+            self.generador_rutas_en_asterisk_conf.create_config_asterisk()
+        except:
+            logger.exception("SincronizadorDeConfiguracionDeRutaSalienteEnAstDB: error al "
+                             "intentar create_config_asterisk()")
+
+            proceso_ok = False
+            mensaje_error += ("Hubo un inconveniente al crear el archivo de "
+                              "configuracion de rutas de Asterisk. ")
+        if not proceso_ok:
+            raise(RestablecerConfiguracionTelefonicaError(mensaje_error))
+        else:
+            self.config_rutas_file.copy_asterisk()
+
+    def _generar_e_insertar_en_astdb(self):
+        proceso_ok = True
+        mensaje_error = ""
+
+        try:
+            self.generador_rutas_en_astdb.regenerar_familys_rutas()
+        except:
+            logger.exception("SincronizadorDeConfiguracionDeRutaSalienteEnAstDB: error al "
+                             "intentar regenerar_familys_rutas()")
+
+            proceso_ok = False
+            mensaje_error += ("Hubo un inconveniente al insertar los registros de las rutas en "
+                              "la base de datos de Asterisk. ")
+            raise (RestablecerConfiguracionTelefonicaError(mensaje_error))
+
+    def regenerar_rutas_salientes(self):
+        self._generar_y_recargar_archivos_conf_asterisk()
+        self._generar_e_insertar_en_astdb()
--- ominicontacto_app/services/asterisk_database.py
+++ ominicontacto_app/services/asterisk_database.py
@@ -294,8 +294,8 @@ class RutaSalienteFamily(object):
         except AsteriskHttpAsteriskDBError:
             logger.exception("Error al intentar DBDelTree de {0}".format(family))
 
-    def regenerar_familys_agente(self):
-        """regenera la family de los agentes"""
+    def regenerar_familys_rutas(self):
+        """regenera la family de las rutas"""
         self.delete_tree_family("OML/OUTR")
         self.create_familys()
 
