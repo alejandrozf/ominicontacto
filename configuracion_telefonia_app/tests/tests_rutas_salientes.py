afefb97d8751ca9842f33f7c561a22d556846a73
Alejandro Zamora <alejandro.zamora@freetechsolutions.com.ar>
2018-06-22 12:24:53 -0300

    - Se realizan fixes en tests de eliminación - Se termina la unificación de todos los tests de rutas salientes

--- configuracion_telefonia_app/tests/tests_rutas_salientes.py
+++ configuracion_telefonia_app/tests/tests_rutas_salientes.py
@@ -12,11 +12,11 @@ from configuracion_telefonia_app.models import (
     TroncalSIP, RutaSaliente, OrdenTroncal, PatronDeDiscado)
 
 
-class TestEliminaRutasSalientes(OMLBaseTest):
+class TestsRutasSalientes(OMLBaseTest):
     PWD = u'admin123'
 
     def setUp(self, *args, **kwargs):
-        super(TestEliminaRutasSalientes, self).setUp(*args, **kwargs)
+        super(TestsRutasSalientes, self).setUp(*args, **kwargs)
         self._crear_troncales_y_rutas()
 
         self.admin = self.crear_administrador()
@@ -46,19 +46,19 @@ class TestEliminaRutasSalientes(OMLBaseTest):
         url = reverse('eliminar_ruta_saliente', args=[self.ruta_1.id])
         response = self.client.get(url, follow=True)
         self.assertTemplateUsed(response, 'registration/login.html')
-        self.assertEqual(TroncalSIP.objects.count(), 2)
-        self.assertEqual(RutaSaliente.objects.count(), 2)
-        self.assertEqual(PatronDeDiscado.objects.count(), 4)
-        self.assertEqual(OrdenTroncal.objects.count(), 3)
 
     def test_administrador_puede_eliminar(self):
         self.client.login(username=self.admin.username, password=self.PWD)
         url = reverse('eliminar_ruta_saliente', args=[self.ruta_1.id])
-        self.client.get(url, follow=True)
         self.assertEqual(TroncalSIP.objects.count(), 2)
         self.assertEqual(RutaSaliente.objects.count(), 2)
         self.assertEqual(PatronDeDiscado.objects.count(), 4)
         self.assertEqual(OrdenTroncal.objects.count(), 3)
+        self.client.post(url, follow=True)
+        self.assertEqual(TroncalSIP.objects.count(), 2)
+        self.assertEqual(RutaSaliente.objects.count(), 1)
+        self.assertEqual(PatronDeDiscado.objects.count(), 2)
+        self.assertEqual(OrdenTroncal.objects.count(), 1)
 
     def test_lista_troncales_huerfanos(self):
         self.client.login(username=self.admin.username, password=self.PWD)
@@ -94,3 +94,83 @@ class TestEliminaRutasSalientes(OMLBaseTest):
         self.assertEqual(RutaSaliente.objects.count(), 2)
         self.assertEqual(PatronDeDiscado.objects.count(), 4)
         self.assertEqual(OrdenTroncal.objects.count(), 3)
+
+    def test_supervisor_normal_no_puede_crear_ruta_saliente(self):
+        usr_sup = self.crear_user_supervisor()
+        self.crear_supervisor_profile(usr_sup)
+        usr_sup.set_password(self.PWD)
+        self.client.login(username=usr_sup.username, password=self.PWD)
+        url = reverse('crear_ruta_saliente')
+        response = self.client.get(url, follow=True)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_supervisor_normal_no_puede_modificar_ruta_saliente(self):
+        usr_sup = self.crear_user_supervisor()
+        self.crear_supervisor_profile(usr_sup)
+        usr_sup.set_password(self.PWD)
+        self.client.login(username=usr_sup.username, password=self.PWD)
+        url = reverse('editar_ruta_saliente', args=[self.ruta_1.id])
+        post_data = {}
+        response = self.client.post(url, post_data, follow=True)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_administrador_puede_crear_ruta_saliente(self):
+        self.client.login(username=self.admin.username, password=self.PWD)
+        post_data = {
+            'nombre': 'test',
+            'ring_time': 1,
+            'dial_options': 'Tt',
+            'patron_discado-0-prepend': 3,
+            'patron_discado-0-prefix': 4,
+            'patron_discado-0-match_pattern': '5',
+            'patron_discado-1-prepend': 6,
+            'patron_discado-1-prefix': 7,
+            'patron_discado-1-match_pattern': '8',
+            'orden_troncal-0-troncal': 1,
+            'orden_troncal-1-troncal': 2,
+            'patron_discado-TOTAL_FORMS': 2,
+            'patron_discado-INITIAL_FORMS': 0,
+            'patron_discado-MIN_NUM_FORMS': 1,
+            'patron_discado-MAX_NUM_FORMS': 1000,
+            'orden_troncal-TOTAL_FORMS': 2,
+            'orden_troncal-INITIAL_FORMS': 0,
+            'orden_troncal-MIN_NUM_FORMS': 1,
+            'orden_troncal-MAX_NUM_FORMS': 1000,
+        }
+        url = reverse('crear_ruta_saliente')
+        n_rutas_salientes = RutaSaliente.objects.count()
+        self.client.post(url, post_data, follow=True)
+        self.assertEqual(RutaSaliente.objects.count(), n_rutas_salientes + 1)
+
+    def test_administrador_puede_modificar_ruta_saliente(self):
+        self.client.login(username=self.admin.username, password=self.PWD)
+        nuevo_nombre = 'ruta_1_modificada'
+        post_data = {
+            'nombre': nuevo_nombre,
+            'ring_time': 4,
+            'dial_options': 'Tt',
+            'patron_discado-0-prepend': 23,
+            'patron_discado-0-prefix': 2,
+            'patron_discado-0-match_pattern': '3',
+            'patron_discado-0-id': self.patron_1_1.pk,
+            'patron_discado-1-prepend': 2,
+            'patron_discado-1-prefix': 1,
+            'patron_discado-1-match_pattern': '2',
+            'patron_discado-1-id': self.patron_1_2.pk,
+            'orden_troncal-0-troncal': 1,
+            'orden_troncal-0-id': self.orden_1_1.pk,
+            'orden_troncal-1-troncal': 4,
+            'orden_troncal-1-id': self.orden_1_2.pk,
+            'patron_discado-TOTAL_FORMS': 2,
+            'patron_discado-INITIAL_FORMS': 2,
+            'patron_discado-MIN_NUM_FORMS': 1,
+            'patron_discado-MAX_NUM_FORMS': 1000,
+            'orden_troncal-TOTAL_FORMS': 2,
+            'orden_troncal-INITIAL_FORMS': 2,
+            'orden_troncal-MIN_NUM_FORMS': 1,
+            'orden_troncal-MAX_NUM_FORMS': 1000,
+        }
+        url = reverse('editar_ruta_saliente', args=[self.ruta_1.pk])
+        self.client.post(url, post_data, follow=True)
+        self.ruta_1.refresh_from_db()
+        self.assertEqual(self.ruta_1.nombre, nuevo_nombre)
