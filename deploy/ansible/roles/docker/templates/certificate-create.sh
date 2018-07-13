8c3a120f23959a4619bfa2cf07aaab14ed9d6125
lionite <felipe.macias@freetechsolutions.com.ar>
2018-07-13 10:33:05 -0300

    sigo puliendo esto todavía falta mucho

--- deploy/ansible/roles/docker/templates/certificate-create.sh
+++ deploy/ansible/roles/docker/templates/certificate-create.sh
@@ -6,19 +6,23 @@ CA_SUBJECT_NODE="/C=AR/ST=CBA/L=CBA/O=FTS/OU=Desarrollo/CN={{ omni_ip }}"
 sed -i "s/\(^DNS.1 =\).*/DNS.1 = {{ omnicentos_fqdn }}/" /etc/pki/tls/openssl.cnf
 sed -i "s/\(^IP.1 =\).*/IP.1 = {{ omni_ip }}/" /etc/pki/tls/openssl.cnf
 
-mkdir {{ kamailio_location }}/etc/certs && chmod 0700 {{ kamailio_location }}/etc/certs
-mkdir {{ kamailio_location }}/etc/certs/demoCA && chmod 0700 {{ kamailio_location }}/etc/certs
-mkdir {{ kamailio_location }}/etc/certs/demoCA/newcerts && chmod 0700 {{ kamailio_location }}/etc/certs
-touch {{ kamailio_location }}/etc/certs/demoCA/index.txt && chmod 0755 {{ kamailio_location }}/etc/certs/demoCA/index.txt
-touch {{ kamailio_location }}/etc/certs/demoCA/serial && echo "01" > {{ kamailio_location }}/etc/certs/demoCA/serial
-cd {{ kamailio_location }}/etc/certs/demoCA/ && \
-   openssl req -new -x509 -extensions v3_ca -keyout key.pem -out cert.pem -days 3650 -passout pass:toor123 -subj "$CA_SUBJECT_CA"
-cd {{ kamailio_location }}/etc/certs/ && \
-   openssl req -new -nodes -keyout key.pem -out req.pem -subj "$CA_SUBJECT_NODE"
-cd {{ kamailio_location }}/etc/certs && \
-   openssl ca -extensions v3_req  -batch -days 1460 -out cert.pem -keyfile demoCA/key.pem -cert demoCA/cert.pem -passin pass:toor123 -infiles req.pem
-chown kamailio:kamailio -R {{ kamailio_location }}/etc/certs
+if [ ! -d {{ kamailio_location }}/etc/certs ]; then
+  mkdir {{ kamailio_location }}/etc/certs && chmod 0700 {{ kamailio_location }}/etc/certs
+  mkdir {{ kamailio_location }}/etc/certs/demoCA && chmod 0700 {{ kamailio_location }}/etc/certs
+  mkdir {{ kamailio_location }}/etc/certs/demoCA/newcerts && chmod 0700 {{ kamailio_location }}/etc/certs
+  touch {{ kamailio_location }}/etc/certs/demoCA/index.txt && chmod 0755 {{ kamailio_location }}/etc/certs/demoCA/index.txt
+  touch {{ kamailio_location }}/etc/certs/demoCA/serial && echo "01" > {{ kamailio_location }}/etc/certs/demoCA/serial
+  cd {{ kamailio_location }}/etc/certs/demoCA/ && \
+  openssl req -new -x509 -extensions v3_ca -keyout key.pem -out cert.pem -days 3650 -passout pass:toor123 -subj "$CA_SUBJECT_CA"
+  cd {{ kamailio_location }}/etc/certs/ && \
+  openssl req -new -nodes -keyout key.pem -out req.pem -subj "$CA_SUBJECT_NODE"
+  cd {{ kamailio_location }}/etc/certs && \
+  openssl ca -extensions v3_req  -batch -days 1460 -out cert.pem -keyfile demoCA/key.pem -cert demoCA/cert.pem -passin pass:toor123 -infiles req.pem
+  chown {{ usuario }}:{{ usuario }} -R {{ kamailio_location }}/etc/certs
+  cat {{ kamailio_location }}/etc/certs/demoCA/cert.pem > /tmp/voip.cert
+  cat {{ kamailio_location }}/etc/certs/cert.pem >> /tmp/voip.cert
 
+fi
 cat > /etc/sysconfig/rtpengine <<EOF
 
 #
@@ -43,6 +47,7 @@ cat > {{ kamailio_location }}/etc/kamailio/kamailio-local.cfg <<EOF
 #!substdef "!MY_ASTERISK!{{ omni_ip }}!g"
 #!substdef "!MY_DB!127.0.0.1!g"
 
+
 #!substdef "!MY_UDP_ADDR!udp:MY_IP_ADDR:MY_UDP_PORT!g"
 #!substdef "!MY_TCP_ADDR!tcp:MY_IP_ADDR:MY_TCP_PORT!g"
 #!substdef "!MY_TLS_ADDR!tls:MY_IP_ADDR:MY_TLS_PORT!g"
@@ -52,4 +57,8 @@ cat > {{ kamailio_location }}/etc/kamailio/kamailio-local.cfg <<EOF
 #!substdef "!MY_MSRPTCP_ADDR!tcp:MY_IP_ADDR:MY_MSRPTCP_PORT!g"
 #!substdef "!MSRP_MIN_EXPIRES!1800!g"
 #!substdef "!MSRP_MAX_EXPIRES!3600!g"
+#!substdef "!MODULES_LOCATION!{{ kamailio_location }}/lib64/kamailio/modules/!g"
+#!substdef "!PKEY_LOCATION!{{ kamailio_location }}/etc/certs/key.pem!g"
+#!substdef "!CERT_LOCATION!{{ kamailio_location }}/etc/certs/cert.pem!g"
+#!substdef "!CA_LOCATION!{{ kamailio_location }}/etc/certs/demoCA/cert.pem!g"
 EOF
--- deploy/ansible/roles/docker/templates/run-docker.sh
+++ deploy/ansible/roles/docker/templates/run-docker.sh
@@ -3,23 +3,28 @@
 COMPOSE_HTTP_TIMEOUT=200 /usr/local/bin/docker-compose up --build -d
 ASTERISK_CONTAINER_ID=`docker ps -a |grep asterisk-omnileads |awk -F " " '{print $1}'`
 KAMAILIO_CONTAINER_ID=`docker ps -a |grep rtpengine-kamailio |awk -F " " '{print $1}'`
+asterisk_location={{ asterisk_location }}
+kamailio_location={{ kamailio_location }}
 
 #Archivos de configuracion asterisk para ir en el container de asterisk
 #docker cp {{ install_prefix }}ominicontacto/asterisk-files/archivos-no-enlaces/*.conf asterisk:/opt/omnileads/asterisk/etc/asterisk/
 #docker cp {{ install_prefix }}ominicontacto/asterisk-files/*.conf asterisk:/opt/omnileads/asterisk/etc/asterisk/
-docker cp {{ install_prefix }}ominicontacto/ominicontacto_voip/extra-files/odbc.ini asterisk:/etc/
-docker cp {{ install_prefix }}ominicontacto/ominicontacto_voip/extra-files/odbcinst.ini asterisk:/etc/
-docker cp {{ install_prefix }}ominicontacto/ominicontacto_voip/asterisk-files/cron-omnileads asterisk:/var/spool/cron/
-docker cp /tmp/conversor.sh asterisk:{{ install_prefix }}bin
+
+cat {{ install_prefix }}ominicontacto/ominicontacto_voip/extra-files/odbc.ini | sudo docker exec -i $ASTERISK_CONTAINER_ID /bin/bash -c 'cat > /etc/odbc.ini'
+cat {{ install_prefix }}ominicontacto/ominicontacto_voip/extra-files/odbcinst.ini  | sudo docker exec -i $ASTERISK_CONTAINER_ID /bin/bash -c 'cat > /etc/odbcinst.ini'
+cat {{ install_prefix }}ominicontacto/ominicontacto_voip/asterisk-files/cron-omnileads  | sudo docker exec -i $ASTERISK_CONTAINER_ID /bin/bash -c 'cat > /var/spool/cron/cron-omnileads'
+cat /tmp/conversor.sh  | sudo docker exec -i $ASTERISK_CONTAINER_ID /bin/bash -c 'cat > {{ install_prefix }}bin'
 
 #Archivos de configuracion kamailio para ir en el container de kamailioro
-docker cp /tmp/kamailio.service kamailio:/etc/systemd/system/
-docker cp /tmp/kamailio kamailio:/etc/default/
-docker cp {{ install_prefix }}bin/certificate-create.sh kamailio:/tmp/
+cat /tmp/openssl.cnf |sudo docker exec -i $KAMAILIO_CONTAINER_ID /bin/bash -c 'cat > /etc/pki/tls/openssl.cnf'
+cat {{ install_prefix }}bin/certificate-create.sh |sudo docker exec -i $KAMAILIO_CONTAINER_ID /bin/bash -c 'cat > /tmp/certificate-create.sh'
 
-docker restart asterisk
-docker exec -i -t $ASTERISK_CONTAINER_ID /bin/bash -c "service asterisk start"
-docker exec -i -t $KAMAILIO_CONTAINER_ID /bin/bash -c "cd /tmp/ && ./certificate-create.sh && \
-                                                          systemctl daemon reload && \
-                                                          systemctl start rtpengine && systemctl enable rtpengine && \
-                                                          systemctl enable kamailio && systemctl start kamailio"
+docker exec -i $ASTERISK_CONTAINER_ID /bin/bash -c "chown -R omnileads. $asterisk_location && service asterisk stop && service asterisk start "
+docker exec -i $KAMAILIO_CONTAINER_ID /bin/bash -c "chown -R omnileads. $kamailio_location && cd /tmp/ && \
+                                                    echo 'y'| $kamailio_location/sbin/kamdbctl create && \
+                                                    chmod +x certificate_create.sh && ./certificate-create.sh && \
+                                                    systemctl start kamailio"
+docker cp kamailio:{{ kamailio_location }}/etc/certs/cert.pem {{ install_prefix }}nginx_certs/
+docker cp kamailio:{{ kamailio_location }}/etc/certs/key.pem {{ install_prefix }}nginx_certs/
+docker cp kamailio:/tmp/voip.cert {{ install_prefix }}static/ominicontacto/voip.cert
+chown -R omnileads. {{ install_prefix }}nginx_certs
