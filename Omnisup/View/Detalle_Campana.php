a63b3d1e25caed6b6d51fd700e6bc3b33a2c3894
Max <maxi.dgr@gmail.com>
2018-08-02 18:49:16 +0200

    Cambios en detalle de supervision. Agregado de tabs.

--- Omnisup/View/Detalle_Campana.php
+++ Omnisup/View/Detalle_Campana.php
@@ -79,81 +79,338 @@
     </div>
 </div>
 
-<div class="jumbotron">
-    <div class="row">
-        <div class="col-sm-3">
-            <h1 class="display-2">12<span id="objcampana"></span> <span id="gestioncampana"></span></h1>
-            <h3>Avance de objetivo</h3>
-        </div>
-        <div class="col-sm-3">
-            <h1 class="display-2">12%<span id="percent"></span></h1>
-            <h3>Porcentaje de objetivo</h3>
-        </div>
-    </div>
-</div>
+<button style="position:absolute; top:30px; right:50px;" type="button" class="btn btn-outline-primary btn-sm" alt="openWebPhone" id="webphone">webphone</button>
 
-<div class="row">
-    <button alt="openWebPhone" id="webphone">webphone</button>
-    <hr>
-    <br>
-    <br>
-</div>
+<ul class="nav nav-tabs" id="supervisionTabBar" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="messagesTab" data-toggle="tab" href="#messagesContent" role="tab" aria-controls="home" aria-selected="true">Uno</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="registryTab" data-toggle="tab" href="#registryContent" role="tab" aria-controls="profile" aria-selected="false">Dos</a>
+  </li>
+</ul>
 
-<div class="row">
-    <div class="col-md-6"><!-- CUADRO RESUMEN CAMPANA -->
-      <h2>Resumen y calificaciones</h2>
-      <table id="tableCampSummary" class="table">
-          <tbody id="bodySummary">
-          </tbody>
-          <tr>
-              <td>Llamadas:</td>
-              <td></td>
-          </tr>
-          <tbody id="bodyScore">
-          </tbody>
-      </table>
-    </div>
-    <div class="col-md-6"><!-- CUADRO AGENTES -->
-        <h2>Agentes</h2>
-        <table id="tableAgt" class="table">
-            <thead>
+<div class="tab-content" id="messagesTabContent">
+
+  <div class="tab-pane fade show active" id="messagesContent" role="tabpanel" aria-labelledby="messagesTab">
+
+      <div class="col-md-6"><!-- CUADRO AGENTES -->
+          <h2>Agentes</h2>
+          <table id="tableAgt" class="table table-stripped table-sm">
+              <thead>
+                  <tr>
+                      <th>Agentes</th>
+                      <th>Estado</th>
+                      <th>Tiempo</th>
+                      <th>Acciones</th>
+                  </tr>
+              </thead>
+              <tbody id="tableAgBody">
                 <tr>
-                    <th>Agentes</th>
-                    <th>Estado</th>
-                    <th>Tiempo</th>
-                    <th>Acciones</th>
+                  <td>felipem</td>
+                  <td>DIALING</td>
+                  <td>23:28:33</td>
+                  <td>
+                      <div class="dropdown">
+                          <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Opciones
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-eye"></span>Monitorear
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Hablar con agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-control-pause"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Log off agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Tomar llamada
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Conferencia
+                              </a>
+                          </div>
+                      </div>
+                  </td>
                 </tr>
-            </thead>
-            <tbody id="tableAgBody">
-            </tbody>
-        </table>
-    </div>
-</div>
-<hr class="hr-space">
+                <tr>
+                  <td>Pepe</td>
+                  <td>PAUSE - Sanitario</td>
+                  <td>23:28:33</td>
+                  <td>
+                      <div class="dropdown">
+                          <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Opciones
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-eye"></span>Monitorear
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Hablar con agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-control-pause"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Log off agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Tomar llamada
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Conferencia
+                              </a>
+                          </div>
+                      </div>
+                  </td>
+                </tr>
+                <tr role="row" class="odd">
+                  <td>AgenteTest</td><td>OFFLINE</td><td>23:28:33</td>
+                  <td>
+                      <div class="dropdown">
+                          <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Opciones
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-eye"></span>Monitorear
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Hablar con agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-control-pause"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Log off agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Tomar llamada
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Conferencia
+                              </a>
+                          </div>
+                      </div>
+                  </td>
+                </tr>
+                <tr role="row" class="odd">
+                  <td>Fulano</td><td>READY</td><td>23:28:33</td>
+                  <td>
+                      <div class="dropdown">
+                          <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Opciones
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-eye"></span>Monitorear
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Hablar con agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-control-pause"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Log off agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Tomar llamada
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Conferencia
+                              </a>
+                          </div>
+                      </div>
+                  </td>
+                </tr>
+                <tr role="row" class="odd">
+                  <td>ABCDEG</td><td>ONCALL</td><td>23:28:33</td>
+                  <td>
+                      <div class="dropdown">
+                          <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Opciones
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-eye"></span>Monitorear
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Hablar con agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-control-pause"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Pausar agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Log off agente
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Tomar llamada
+                              </a>
+                              <a class="dropdown-item" href="#">
+                                  <span class="icon icon-pencil"></span>Conferencia
+                              </a>
+                          </div>
+                      </div>
+                  </td>
+                </tr>
+              </tbody>
+          </table>
+      </div>
+
+      <div class="row">
+          <div class="col-md-6"><!-- CUADRO CANALES -->
+            <hr>
+            <h2>Estado general de lineas</h2>
+            <table id="" class="table table-sm">
+                <thead>
+                    <tr>
+                        <th>Estado</th><th>Destino</th>
+                    </tr>
+                </thead>
+                <tbody id="tableChannelsWombat">
+                  <tr>
+                    <td>Connected</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Connected</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Connected</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Calling</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Calling</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>ShortCall</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Connected</td><td>3516285260</td>
+                  </tr>
+                  <tr>
+                    <td>Calling</td><td>3514215610</td>
+                  </tr>
+                  <tr>
+                    <td>ShortCall</td><td>3516285260</td>
+                  </tr>
+                </tbody>
+
+            </table>
+          </div>
+          <div class="col-md-6"><!-- CUADRO LLAMADAS EN COLA -->
+              <hr>
+            <h2>Llamadas en espera</h2>
+            <table class="table table-sm">
+                <thead>
+                  <tr>
+                      <th>Tiempo</th>
+                      <th>#</th>
+                  </tr>
+                </thead>
+                <tbody id="tableQueuedCalls">
+                    <tr>
+                      <td>00:02:56</td><td>4553131</td>
+                    </tr>
+                    <tr>
+                      <td>00:00:34</td><td>4328977</td>
+                    </tr>
+                    <tr>
+                      <td>00:01:09</td><td>4149090</td>
+                    </tr>
+                    <tr>
+                      <td>00:05:12</td><td>4287920</td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+      </div>
+  </div>
+
+
+  <div class="tab-pane fade" id="registryContent" role="tabpanel" aria-labelledby="registryTab">
+
+      <div class="jumbotron">
+          <div class="row">
+              <div class="col-sm-3">
+                  <h1 class="display-2">12<span id="objcampana"></span> <span id="gestioncampana"></span></h1>
+                  <h3>Avance de objetivo</h3>
+              </div>
+              <div class="col-sm-3">
+                  <h1 class="display-2">12%<span id="percent"></span></h1>
+                  <h3>Porcentaje de objetivo</h3>
+              </div>
+          </div>
+      </div>
+
 <div class="row">
-    <div class="col-md-6"><!-- CUADRO CANALES -->
-      <h2>Estado general de lineas</h2>
-      <table id="" class="table table-striped table-condensed">
-          <thead>
-              <tr>
-                  <th>Estado</th><th>Destino</th>
-              </tr>
-          </thead>
-          <tbody id="tableChannelsWombat">
-          </tbody>
-      </table>
-    </div>
-    <div class="col-md-6"><!-- CUADRO LLAMADAS EN COLA -->
-      <h2>Llamadas en espera</h2>
-      <table class="table">
-          <thead>
-            <tr>
-                <th>Tiempo</th>
-                <th>#</th>
-            </tr>
-          </thead>
-          <tbody id="tableQueuedCalls">
-          </tbody>
-      </table>
-    </div>
+
+</div>
+      <div class="row">
+          <div class="col-md-6"><!-- CUADRO RESUMEN CAMPANA -->
+            <h2>Resumen y calificaciones</h2>
+            <table id="tableCampSummary" class="table table-sm">
+                <tbody id="bodySummary">
+                  <tr>
+                    <td>Agendados</td><td>2374</td>
+                  </tr>
+                  <tr>
+                    <td>bonito</td><td>1148</td>
+                  </tr>
+                  <tr>
+                    <td>Venta</td><td>995</td>
+                  </tr>
+                  <tr>
+                    <td>barato</td><td>3286</td>
+                  </tr>
+                  <tr>
+                    <td>Agendado</td><td>787</td>
+                  </tr>
+                </tbody>
+            </table>
+          </div>
+          <div class="col-md-6">
+              <h2>Llamadas</h2>
+              <div class="">
+                  0<span class="label">Recibidas</span>,
+                  0<span class="label">Atendidas</span>,
+                  0<span class="label">Abandonadas</span>,
+                  0<span class="label">Expiradas</span>,
+                  0<span class="label">Espera</span>,
+                  0<span class="label">Abandono</span>
+              </div>
+              <hr>
+              <h2>Llamadas Manuales</h2>
+              <div class="">
+                  0<span class="label">Efectuadas</span>,
+                  0<span class="label">Conectadas</span>,
+                  0<span class="label">No conec.</span>,
+                  0<span class="label">Espera prom.</span>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
