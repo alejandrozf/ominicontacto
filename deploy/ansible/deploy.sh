c82618220758035ca588d09949e42a64f00e1675
lionite <felipe.macias@freetechsolutions.com.ar>
2018-08-14 16:52:23 -0300

    pulo unas cositas de mariadb y supervision, en el dockerfile me faltaba instalar php, por eso no se ejecutaba el script de grabaciones

--- deploy/ansible/deploy.sh
+++ deploy/ansible/deploy.sh
@@ -193,7 +193,7 @@ Tag() {
     echo "Reflexionar serena, muy serenamente, es mejor que tomar decisiones desesperadas - Franz Kafka"
     echo ""
     if [ $DOCKER == "true" ]; then
-      ${IS_ANSIBLE}-playbook -s $TMP_ANSIBLE/docker.yml --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}" -K -vvvvvvvvvvvv
+      ${IS_ANSIBLE}-playbook -s $TMP_ANSIBLE/docker.yml --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}" -K 
     else
       ${IS_ANSIBLE}-playbook -s $TMP_ANSIBLE/omnileads.yml --extra-vars "BUILD_DIR=$TMP/ominicontacto RAMA=$rama" --tags "${array[0]},${array[1]}" --skip-tags "${array[2]}" -K
 
