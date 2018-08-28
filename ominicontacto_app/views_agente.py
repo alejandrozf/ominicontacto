0f7324a35d689aa4c92eceb3c73d8aba638258b6
Federico Peker <federicopeker@gmail.com>
2018-08-28 14:59:05 -0300

    cambia context originate logut agente

--- ominicontacto_app/views_agente.py
+++ ominicontacto_app/views_agente.py
@@ -141,7 +141,7 @@ def logout_view(request):
         try:
             client = AsteriskHttpClient()
             client.login()
-            client.originate("Local/066LOGOUT@fts-pausas/n", "oml-agent-actions", True,
+            client.originate("Local/066LOGOUT@oml-agent-actions/n", "oml-agent-actions", True,
                              variables, True, aplication='Hangup')
 
         except AsteriskHttpOriginateError:
