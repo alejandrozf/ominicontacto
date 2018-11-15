f1fba656ac846cfc0a84691b3f02891e77b4c93a
Alejandro Zamora Fonseca <alejandro.zamora@freetechsolutions.com.ar>
2018-11-15 11:07:33 -0300

    - Fix en obtención campos audios form IVR

--- configuracion_telefonia_app/forms.py
+++ configuracion_telefonia_app/forms.py
@@ -334,13 +334,13 @@ class IVRForm(forms.ModelForm):
         cleaned_data = super(IVRForm, self).clean()
         audio_ppal_escoger = cleaned_data['audio_ppal_escoger']
         audio_principal = cleaned_data.get('audio_principal', None)
-        audio_ppal_ext_audio = cleaned_data['audio_ppal_ext_audio']
+        audio_ppal_ext_audio = cleaned_data.get('audio_ppal_ext_audio', None)
         time_out_audio_escoger = cleaned_data['time_out_audio_escoger']
-        time_out_audio = cleaned_data['time_out_audio']
-        time_out_ext_audio = cleaned_data['time_out_ext_audio']
+        time_out_audio = cleaned_data.get('time_out_audio', None)
+        time_out_ext_audio = cleaned_data.get('time_out_ext_audio', None)
         invalid_destination_audio_escoger = cleaned_data['invalid_destination_audio_escoger']
-        invalid_audio = cleaned_data['invalid_audio']
-        invalid_destination_ext_audio = cleaned_data['invalid_destination_ext_audio']
+        invalid_audio = cleaned_data.get('invalid_audio', None)
+        invalid_destination_ext_audio = cleaned_data.get('invalid_destination_ext_audio', None)
         self._validar_escoger_audio(audio_ppal_escoger, audio_principal, audio_ppal_ext_audio,
                                     obligatorio=True)
         self._validar_escoger_audio(time_out_audio_escoger, time_out_audio, time_out_ext_audio)
