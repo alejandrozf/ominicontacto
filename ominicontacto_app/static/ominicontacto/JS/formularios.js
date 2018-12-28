cb44d5097854b497dec4f6ba7b3d701c947fc814
Alejandro Zamora Fonseca <alejandro.zamora@freetechsolutions.com.ar>
2018-12-28 16:09:29 -0300

    - Añade header de licencia GPL - Fix en UI - Refactor JS

--- ominicontacto_app/static/ominicontacto/JS/formularios.js
+++ ominicontacto_app/static/ominicontacto/JS/formularios.js
@@ -1,3 +1,22 @@
+/* Copyright (C) 2018 Freetech Solutions
+
+ This file is part of OMniLeads
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see http://www.gnu.org/licenses/.
+
+/
+
 /*
  * Script que realiza bindings a todos los botones de mostrar/ocultar en la lista de formularios,
  * en cada click actualiza el estado del atributo 'oculto' de formulario en cuestión en BD y modifica convenientemente
@@ -30,7 +49,7 @@ function actualizarNodoFormulario($this, data, mostrarOcultos) {
     else {
       // sólo se muestran los 'visibles', se elimina el nodo
       // directamente
-      $this.parent().remove();
+      $this.parents('.formularioElem').remove();
     }
   }
 }
