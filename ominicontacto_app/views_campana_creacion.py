86b65fa009fd9a1008a39df561115c97f18811ef
Alejandro Zamora Fonseca <alejandro.zamora@freetechsolutions.com.ar>
2019-01-21 14:43:46 -0300

    - Soluciona problema de inicializacion de formsets de parametros de   sitio externo en creación campañas desde templates

--- ominicontacto_app/views_campana_creacion.py
+++ ominicontacto_app/views_campana_creacion.py
@@ -132,6 +132,24 @@ class CampanaTemplateCreateCampanaMixin(object):
             calif_init_formset.extra = len(initial_data) - 1
             calif_init_formset.prefix = opts_calif_init_formset.prefix
             context['wizard']['form'] = calif_init_formset
+        if current_step == self.PARAMETROS_CRM:
+            initial_data = campana_template.parametros_crm.values(
+                'tipo', 'valor', 'nombre')
+            bd_contacto = campana_template.bd_contacto
+            if bd_contacto is None:
+                nombres_de_columnas = COLUMNAS_DB_DEFAULT
+                nombres_de_columnas = nombres_de_columnas[:]
+            else:
+                metadata = bd_contacto.get_metadata()
+                nombres_de_columnas = metadata.nombres_de_columnas
+            nombres_de_columnas.remove('telefono')
+            columnas_bd = [(columna, columna) for columna in nombres_de_columnas]
+            params_crm_init_formset = context['wizard']['form']
+            param_crms_formset = ParametrosCrmFormSet(
+                initial=initial_data, form_kwargs={'columnas_bd': columnas_bd})
+            param_crms_formset.extra = len(initial_data) + 1
+            param_crms_formset.prefix = params_crm_init_formset.prefix
+            context['wizard']['form'] = param_crms_formset
         return context
 
 
