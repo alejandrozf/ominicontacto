995c20195db0044352742e568f1a316a39ef4e49
Felipe Macias <felipe.macias@freetechsolutions.com.ar>
2019-05-02 11:27:03 -0300

    - Divide los requirements para diferenciar libs locales de las de       producción     - Cambia estructura settings     - Se actualizan tareas de ansible     - Se actualiza script de CI Gitlab     - Actualiza devenv docker

--- ominicontacto/wsgi.py
+++ ominicontacto/wsgi.py
@@ -24,10 +24,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ominicontacto.settings")
 
 application = get_wsgi_application()
