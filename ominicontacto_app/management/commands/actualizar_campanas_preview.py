b23f48d2a5c77582da78b6d63ba7851fcfcadcc5
Alejandro Zamora Fonseca <ale2014.zamora@gmail.com>
2019-09-20 20:52:39 +0000

    - Fix en error de unicode - Adiciona mas informacion en el log del comando

--- ominicontacto_app/management/commands/actualizar_campanas_preview.py
+++ ominicontacto_app/management/commands/actualizar_campanas_preview.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 #
+from __future__ import unicode_literals
 
 import logging
 
@@ -59,4 +60,4 @@ class Command(BaseCommand):
         try:
             self._actualizar_relacion_agente_contacto(campana_id, tiempo_desconexion)
         except Exception as e:
-            logging.error('Fallo del comando: {0}'.format(e.message))
+            logging.error('Fallo del comando: {0}'.format(e))
