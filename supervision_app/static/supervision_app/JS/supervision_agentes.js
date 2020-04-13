f7a536e52054b1dfe471c621d71c428d25c32033
Simon R. Moreno <simon.moreno@freetechsolutions.com.ar>
2020-04-13 17:28:37 -0300

    Fix para prevenir que el supervisor deje la pagina de supervision mientras tiene activo un channel spy

--- supervision_app/static/supervision_app/JS/supervision_agentes.js
+++ supervision_app/static/supervision_app/JS/supervision_agentes.js
@@ -30,6 +30,8 @@ $(function (){
     var sipSecret = $('#sipSec').val();
 
     phone_controller = new PhoneJSController(supervisor_id, sipExtension, sipSecret);
+
+    window.addEventListener('beforeunload', preventLeaveOnCall);
 });
 
 function executeSupervisorAction(pk_agent, action) {
@@ -53,4 +55,18 @@ function executeSupervisorAction(pk_agent, action) {
             console.log(gettext('Error al ejecutar => ') + textStatus + ' - ' + errorThrown);
         },
     });    
+}
+
+function preventLeaveOnCall(event) {
+    if (phone_controller.is_on_call()) {
+        $.growl.warning({ 
+            title: gettext('Atención!'),
+            message: gettext('Debe finalizar la acción actual antes salir de esta pantalla.')});
+
+        // Cancel the event as stated by the standard.
+        event.preventDefault();
+        // Chrome requires returnValue to be set.
+        event.returnValue = gettext('Debe finalizar la acción actual antes de salir de esta pantalla.');
+        return gettext('Debe finalizar la acción actual antes de salir de esta pantalla.');
+    }
 }
