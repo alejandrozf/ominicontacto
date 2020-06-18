795dffa50ca354faaed15d11f6a1d473806e8fc5
Alejandro Zamora Fonseca <ale2014.zamora@gmail.com>
2020-06-18 17:50:46 -0300

    - Refactor para optimizar el calculo de todos los reportes de una   campaña, se dejan en un numero fijo de queries - quita tarea  'regenerar_tareas_preview' de deploy - fix liberar_contactos_por_tiempo

--- ominicontacto_app/utiles.py
+++ ominicontacto_app/utiles.py
@@ -272,6 +272,14 @@ def fecha_hora_local(fecha_hora):
     return fecha_hora.astimezone(timezone.get_current_timezone())
 
 
+def datetime_hora_minima_dia_utc(fecha_utc):
+    return fecha_utc.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def datetime_hora_maxima_dia_utc(fecha_utc):
+    return fecha_utc.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+
 def convertir_ascii_string(cadena):
     """ Devuelve ascii ignorando caracteres extraños"""
     return cadena.encode('ascii', errors='ignore').decode('utf-8')
