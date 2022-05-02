be8b4f90b33c302bdf0cf72b975a9360cf763732
Isachi <isachiqba@gmail.com>
2022-05-02 11:37:18 -0500

    [DT] Actualizar urls.py para no utilizar django.conf.urls.url()
    refs OML-2152

--- ominicontacto/urls.py
+++ ominicontacto/urls.py
@@ -22,15 +22,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, re_path
+    2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import include, re_path
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -40,24 +40,24 @@ import os
 js_info_packages = ('ominicontacto_app',)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('ominicontacto_app.urls')),
-    url(r'^', include('reciclado_app.urls')),
-    url(r'^', include('reportes_app.urls')),
-    url(r'^', include('configuracion_telefonia_app.urls')),
-    url(r'^', include('supervision_app.urls')),
-    url(r'^', include('api_app.urls')),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/accounts/login/'),
-        name="logout"),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include('ominicontacto_app.urls')),
+    re_path(r'^', include('reciclado_app.urls')),
+    re_path(r'^', include('reportes_app.urls')),
+    re_path(r'^', include('configuracion_telefonia_app.urls')),
+    re_path(r'^', include('supervision_app.urls')),
+    re_path(r'^', include('api_app.urls')),
+    re_path(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/accounts/login/'),
+            name="logout"),
 ]
 
 for (regex, module) in settings.ADDON_URLPATTERNS:
-    urlpatterns += [url(regex, include(module)), ]
+    urlpatterns += [re_path(regex, include(module)), ]
 
 urlpatterns += [
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=js_info_packages),
-        name='javascript-catalog'),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=js_info_packages),
+            name='javascript-catalog'),
+    re_path(r'^i18n/', include('django.conf.urls.i18n')),
 ]
 
 # TODO me funciona con el if de abajo el if de arriba es mas prolijo pero no funciona
@@ -65,5 +65,5 @@ urlpatterns += [
 if os.getenv('DJANGO_SETTINGS_MODULE') == 'ominicontacto.settings.develop':
     import debug_toolbar
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
