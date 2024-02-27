d61e563f479795033d70e54d5ec4c9ea304bba38
jjlorenzo <jjlorenzo@users.noreply.github.com>
2024-02-27 11:41:20 +0100

    updates to allow theme customizations without app rebuild
    we are currently using a deprecated tooling for vue project, so we try to keep
    the dependencies on that minimal.

--- omnileads_ui/src/main.js
+++ omnileads_ui/src/main.js
@@ -9,7 +9,6 @@ import Helpers from '@/helpers';
 // Primevue
 import PrimeVue from 'primevue/config';
 import 'primeflex/primeflex.css';
-import '@/assets/primevue/3.12.0/resources/themes/saga-dark-pink/theme.css'; // theme
 import 'primevue/resources/primevue.min.css'; // core css
 import 'primeicons/primeicons.css'; // icons
 
