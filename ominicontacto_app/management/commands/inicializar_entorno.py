5079ec229d6508913a4ebe67c857db7ed66aaf56
Simon R. Moreno <simon.moreno@freetechsolutions.com.ar>
2024-03-04 18:39:36 -0300

    Inicializar entorno campanas preview

--- ominicontacto_app/management/commands/inicializar_entorno.py
+++ ominicontacto_app/management/commands/inicializar_entorno.py
@@ -136,7 +136,8 @@ class Command(BaseCommand):
         # crear campaña dialer
         campana = CampanaFactory(
             nombre=nombre_campana, bd_contacto=self.bd_contacto,
-            type=Campana.TYPE_DIALER, reported_by=self.admin, estado=Campana.ESTADO_ACTIVA
+            type=Campana.TYPE_DIALER, reported_by=self.admin, estado=Campana.ESTADO_ACTIVA,
+            tiempo_desconexion=10
         )
         # crear Queue para la campaña
         Queue.objects.create(
@@ -161,11 +162,14 @@ class Command(BaseCommand):
 
         return campana
 
-    def _crear_campana_preview(self, nombre_campana):
-        # crear campaña dialer
+    def _crear_campana_preview(self, nombre_campana, bd_contacto, es_template=False):
+        # crear campaña preview
+        estado = Campana.ESTADO_ACTIVA
+        if es_template:
+            estado = Campana.ESTADO_TEMPLATE_ACTIVO
         campana = CampanaFactory(
-            nombre=nombre_campana, bd_contacto=self.bd_contacto,
-            type=Campana.TYPE_PREVIEW, reported_by=self.admin, estado=Campana.ESTADO_ACTIVA
+            nombre=nombre_campana, bd_contacto=bd_contacto,
+            type=Campana.TYPE_PREVIEW, reported_by=self.admin, estado=estado
         )
         # crear Queue para la campaña
         Queue.objects.create(
@@ -187,7 +191,8 @@ class Command(BaseCommand):
         )
         self._crear_opciones_calificacion(campana)
 
-        campana.establecer_valores_iniciales_agente_contacto(False, False)
+        if not es_template:
+            campana.establecer_valores_iniciales_agente_contacto(False, False)
 
         return campana
 
@@ -244,6 +249,52 @@ class Command(BaseCommand):
         cliente_webphone.save()
         return user
 
+    def _crear_dbs_contactos(self):
+        # crear BD default (100 contactos)
+        self.bd_contacto = BaseDatosContactoFactory()
+        ContactoFactory.create_batch(100, bd_contacto=self.bd_contacto)
+
+        # Crear DBs Preview
+        metadata = '{"cant_col": 4, "cols_telefono": [0], ' + \
+            '"nombres_de_columnas": ["telefono", "nombre", "direccion", "localidad"]}'
+        self.bd_contacto_prw1 = BaseDatosContactoFactory(
+            nombre='PRW1', nombre_archivo_importacion='',
+            metadata=metadata, cantidad_contactos=7
+        )
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='123456721',
+                        datos='["Jorge Success", "CORD", "SANLUIS"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='88887777',
+                        datos='["Luis Blacklist","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='9999',
+                        datos='["Graciela No Route","NAVARROSUR1132S","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='123456729',
+                        datos='["Alfredo Congestion","ABERASTAINSUR655","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='123456725',
+                        datos='["Oscar No Answer","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='123456720',
+                        datos='["Cecilia Busy","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw1, telefono='123456725',
+                        datos='["Ricardo Cancel","ABERASTAINSUR655","SANJUAN"]')
+
+        self.bd_contacto_prw_success = BaseDatosContactoFactory(
+            nombre='PRW1-SUCCESS', nombre_archivo_importacion='',
+            metadata=metadata, cantidad_contactos=7
+        )
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456721',
+                        datos='["Jorge Success", "CORD", "SANLUIS"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456731',
+                        datos='["Luis Blacklist","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456741',
+                        datos='["Graciela No Route","NAVARROSUR1132S","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456751',
+                        datos='["Alfredo Congestion","ABERASTAINSUR655","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456761',
+                        datos='["Oscar No Answer","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456771',
+                        datos='["Cecilia Busy","CATAMARCASUR178","SANJUAN"]')
+        ContactoFactory(bd_contacto=self.bd_contacto_prw_success, telefono='123456781',
+                        datos='["Ricardo Cancel","ABERASTAINSUR655","SANJUAN"]')
+
     def _crear_datos_entorno(self, qa_devops):
 
         self.admin = User.objects.filter(is_staff=True).first()
@@ -283,9 +334,7 @@ class Command(BaseCommand):
         self.success = NombreCalificacionFactory(nombre='ventas_Lee PLC')        
         self.angry = NombreCalificacionFactory(nombre='hangup')
 
-        # crear BD (100 contactos)
-        self.bd_contacto = BaseDatosContactoFactory()
-        ContactoFactory.create_batch(100, bd_contacto=self.bd_contacto)
+        self._crear_dbs_contactos()
 
         # Crear campañas
         campana_manual = self._crear_campana_manual('test_manual_01')
@@ -294,7 +343,9 @@ class Command(BaseCommand):
         campana_entrante_2.videocall_habilitada = True
         campana_entrante_2.save()
         campana_dialer = self._crear_campana_dialer('test_dialer_01')
-        campana_preview = self._crear_campana_preview('test_preview_01')
+        campana_preview = self._crear_campana_preview('test_preview_01', self.bd_contacto)
+        self._crear_campana_preview('PRW_TEMPLATE', self.bd_contacto_prw1, True)
+        self._crear_campana_preview('PRW_SUCCESS_TEMPLATE', self.bd_contacto_prw_success, True)
 
         activacion_queue_service = ActivacionQueueService()
         activacion_queue_service.activar()
