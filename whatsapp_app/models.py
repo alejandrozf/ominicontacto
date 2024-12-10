0423a9e62e3faed20b522ac229f57c1818abb568
Isachi <isachiqba@gmail.com>
2024-12-10 17:42:17 +0100

    refs OML-2825

--- whatsapp_app/models.py
+++ whatsapp_app/models.py
@@ -276,11 +276,11 @@ class MenuInteractivoWhatsapp(models.Model):
 
     @property
     def nombre(self):
-        return 'menu-interactivo-whatsapp-{0}'.format(self.id)
+        return self.menu_header
 
 
 class OpcionMenuInteractivoWhatsapp(models.Model):
     opcion = models.OneToOneField(
         'configuracion_telefonia_app.OpcionDestino', on_delete=models.CASCADE,
         related_name="opcion_menu_whatsapp")
-    descripcion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100, null=True, blank=True)
