70c159e184ece4cb5dfd95d13d8b844432601a32
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-03-10 18:05:16 -0300

    - implements a consumer for the omnidialer PUBSUB channel

--- ominicontacto/settings/defaults.py
+++ ominicontacto/settings/defaults.py
@@ -145,6 +145,12 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
+    'redis_pubsub': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('redis', 6379)],
+        },
+    },
 }
 
 # Password hashers available
